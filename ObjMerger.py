20f9e570a4d9e899ee3534113f0ef51940988cc7
lzkelley <lkelley@cfa.harvard.edu>
2014-10-17 14:40:11 -0400

    Modifications to Blackhole and Merger objects

--- ObjMerger.py
+++ ObjMerger.py
@@ -18,24 +18,39 @@ from datetime import datetime
 from ObjBlackhole import Blackhole
 
 
-
 INT        = np.int64
 FLT        = np.float64
-
 
 
 class Merger(object):
     '''Object representing a single merger (or creation) event.'''
 
+    '''
     def __init__(self, instr):
-        args = self.parseMergerLine(instr)                                                          # Extract target quanitites from string
+        args          = self.parseMergerLine(instr)                                                 # Extract target quanitites from string
 
         self.time     = args[0]                                                                     # Simulation time at merger
         self.bhin     = Blackhole(args[3], args[4], args[0])                                        # Accreted BH
         self.bhout    = Blackhole(args[1], args[2], args[0])                                        # Accretor BH
+     '''
+
+    def __init__(self, time, bhin, bhout):
+        self.time  = time
+        self.bhin  = bhin
+        self.bhout = bhout
+
 
     def __str__(self):
         return "Time: %g  In , Out  =  %s , %s" % (self.time, str(self.bhin), str(self.bhout))
+
+
+    @classmethod
+    def fromMergerFileLine(cls, mline):
+        '''Initialize from line of Merger File'''
+        time, inid, inma, outid, outma = cls.parseMergerLine(mline)
+        bhin  = Blackhole(inid, inma, time)
+        bhout = Blackhole(outid, outma, time)
+        return cls(time, bhin, bhout)
 
 
     @staticmethod
