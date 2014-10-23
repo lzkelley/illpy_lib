5b4f2cd6e6c7e2ba1e7b18e020adc1247c3f68c7
lzkelley <lkelley@cfa.harvard.edu>
2014-10-23 18:24:35 -0400

    Completely changed Merger object to apply to a full list of Mergers. Now it is a simple wrapper for a few numpy arrays with added convenience functions.

--- ObjMerger.py
+++ ObjMerger.py
@@ -14,57 +14,136 @@ import readsnapHDF5 as rs
 from glob import glob
 from datetime import datetime
 
-#import LyzeMergers as lm
-from ObjBlackhole import Blackhole
+from BlackholeMergerSettings import *
 
 
-INT        = np.int64
-FLT        = np.float64
 
+class Mergers(object):
+    ''' Class to store many mergers with components as simple arrays.
 
-class Merger(object):
-    '''Object representing a single merger (or creation) event.'''
+    Usage:
+      mergers = ManyMergers(NUM_MERGERS)
+      mergers[i] = [ TIME, OUT_ID, OUT_MASS, IN_ID, IN_MASS ]
+
+    This class is just a wrapper for 5 numpy arrays storing the time,
+    IDs and masses of both blackholes ('in' the accreted, and 'out'
+    the accretor).  Each constituent array can be accessed as if this
+    object was a dict, e.g. 
+      times = mergers[MERGER_TIME]
+    or the values for a single merger can be accessed, e.g.
+      merger100 = mergers[100]
+
+    ManyMergers supports deletion by element, or series of elements,
+    e.g.
+      del mergers[100]
+      mergers.delete([100,200,221])
+    
+    Individual mergers can be added using either the ManyMergers.add()
+    method, or by accessing the last+1 memeber, e.g. if len(mergers) = N
+      mergers[N+1] = [ TIME, IN_ID, ... ]
+    will also work.
 
     '''
-    def __init__(self, instr):
-        args          = self.parseMergerLine(instr)                                                 # Extract target quanitites from string
 
-        self.time     = args[0]                                                                     # Simulation time at merger
-        self.bhin     = Blackhole(args[3], args[4], args[0])                                        # Accreted BH
-        self.bhout    = Blackhole(args[1], args[2], args[0])                                        # Accretor BH
-     '''
+    MERGER_TIME     = 'time'
+    MERGER_OUT_ID   = 'out_id'
+    MERGER_OUT_MASS = 'out_mass'
+    MERGER_IN_ID    = 'in_id'
+    MERGER_IN_MASS  = 'in_mass'
 
-    def __init__(self, time, bhin, bhout):
-        self.time  = time
-        self.bhin  = bhin
-        self.bhout = bhout
-
-
-    def __str__(self):
-        return "Time: %g  In , Out  =  %s , %s" % (self.time, str(self.bhin), str(self.bhout))
-
-
-    @classmethod
-    def fromMergerFileLine(cls, mline):
-        '''Initialize from line of Merger File'''
-        time, inid, inma, outid, outma = cls.parseMergerLine(mline)
-        bhin  = Blackhole(inid, inma, time)
-        bhout = Blackhole(outid, outma, time)
-        return cls(time, bhin, bhout)
+    
+    def __init__(self, nums):
+        ''' Initialize object with empty arrays for 'num' entries '''
+        self.time     = np.zeros(nums, dtype=FLT )
+        self.out_id   = np.zeros(nums, dtype=UINT)
+        self.out_mass = np.zeros(nums, dtype=FLT )
+        self.in_id    = np.zeros(nums, dtype=UINT)
+        self.in_mass  = np.zeros(nums, dtype=FLT )
+        self.__len    = nums
 
 
-    @staticmethod
-    def parseMergerLine(instr):
+    def __len__(self): return self.__len
+
+
+    def __getitem__(self, key):
         '''
-        Parse a line from an Illustris blachole_mergers_#.txt file
+        Return target arrays.
 
-        The line is formatted (in C) as:
-            '%d %g %llu %g %llu %g\n', 
-            ThisTask, All.Time, (long long) id,  mass, (long long) P[no].ID, BPP(no).BH_Mass
-
-        return time, accretor_id, accretor_mass, accreted_id, accreted_mass
+        For a particular merger, key should be that index.
+        For a particular array, key is appropriate string argument.
+            e.g. MERGER_TIME = 'time' for the time array
         '''
+
+        if(   type(key) == int ): 
+            return [ self.time[key], self.in_id[key], self.in_mass[key], 
+                     self.out_id[key], self.out_mass[key] ]
+        elif( key == MERGER_TIME     ): return self.time
+        elif( key == MERGER_OUT_ID   ): return self.out_id
+        elif( key == MERGER_OUT_MASS ): return self.out_mass
+        elif( key == MERGER_IN_ID    ): return self.in_id
+        elif( key == MERGER_IN_MASS  ): return self.in_mass
+        else: raise KeyError("Unrecozgnized key '%s'!" % (str(key)) )
+
+
+    def __setitem__(self, key, vals):
+        '''
+        Set target array.
+
+        For a particular merger, key should be that index.
+        For a particular array, key is appropriate string argument.
+            e.g. MERGER_TIME = 'time' for the time array
+        '''
+        if(   type(key) == int ): 
+            if( key == self.__len ): self.add(vals)
+            else: 
+                self.time[key]     = vals[0]
+                self.out_id[key]   = vals[1]
+                self.out_mass[key] = vals[2]
+                self.in_id[key]    = vals[3]
+                self.in_mass[key]  = vals[4]
+        elif( key == MERGER_TIME     ): 
+            self.time = vals
+        elif( key == MERGER_OUT_ID   ): 
+            self.out_id = vals
+        elif( key == MERGER_OUT_MASS ): 
+            self.out_mass = vals
+        elif( key == MERGER_IN_ID    ): 
+            self.in_id = vals
+        elif( key == MERGER_IN_MASS  ): 
+            self.in_mass = vals
+        else: raise KeyError("Unrecozgnized key '%s'!" % (str(key)) )
+
+
+    def __delitem__(self, key):
+        ''' Delete the merger array at the target index '''
+
+        self.time     = np.delete(self.time,     key)
+        self.out_id   = np.delete(self.out_id,   key)
+        self.out_mass = np.delete(self.out_mass, key)
+        self.in_id    = np.delete(self.in_id,    key)
+        self.in_mass  = np.delete(self.in_mass,  key)
+        self.__len    = len(self.time)
+
+
+    def delete(self, keys):
+        ''' Delete the merger(s) at 'keys' - an integer (list) '''
+
+        self.time     = np.delete(self.time,     keys)
+        self.out_id   = np.delete(self.out_id,   keys)
+        self.out_mass = np.delete(self.out_mass, keys)
+        self.in_id    = np.delete(self.in_id,    keys)
+        self.in_mass  = np.delete(self.in_mass,  keys)
+        self.__len    = len(self.time)
+
         
-        args = instr.split()
-        return FLT(args[1]), INT(args[2]), FLT(args[3]), INT(args[4]), FLT(args[5])  
+    def add(self, vals):
+        ''' Append the given merger information as a new last element '''
 
+        self.time     = np.append(self.time,     vals[0])
+        self.out_id   = np.append(self.out_id,   vals[3])
+        self.out_mass = np.append(self.out_mass, vals[4])
+        self.in_id    = np.append(self.in_id,    vals[1])
+        self.in_mass  = np.append(self.in_mass,  vals[2])
+        self.__len    = len(self.time)
+
+    
