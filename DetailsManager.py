67d6213d4964175dbec7d555766fcf2be80f4572
lzkelley <lkelley@cfa.harvard.edu>
2014-11-28 18:31:06 -0500

    Significant cleanup / improvement on DetailsManager.py --- don't think this was actually working before.  Switched to simplified strategy using NPZ resultant files.

--- DetailsManager.py
+++ DetailsManager.py
@@ -1,16 +1,15 @@
 # ==================================================================================================
 # DetailsManager.py
 # -----------------
-# 
-# 
 #
-# 
-# 
 #
-# Contains
-# --------
-# + 
 #
+#
+#
+#
+# Illustris BH Details Files:
+# 0   1          2    3     4  5
+# ID scalefactor mass mdot rho cs
 #
 #
 # ------------------
@@ -39,9 +38,22 @@ sys.path.append(*LIB_PATHS)
 from Constants import *
 
 # Import local project files and objects
-#from ObjDetails import Details
 import AuxFuncs as aux
 
+
+PP_BH_DETAILS_DIR = "/n/home00/lkelley/illustris/post-process/Illustris-%d/bh-details/"
+PP_BH_DETAILS_ASCII_FILENAME = "ill-%d_details_ascii_%03d.dat"
+PP_BH_DETAILS_NPZ_FILENAME = "ill-%d_details_npz_%03d.npz"
+
+### Dictionary Keys for Details Parametesr ###
+DET_ID    = 'id'
+DET_SCALE = 'scale'
+DET_MASS  = 'mass'
+DET_MDOT  = 'mdot'
+DET_RHO   = 'rho'
+DET_CS    = 'cs'
+
+DET_NUM   = 6
 
 
 
@@ -51,7 +63,7 @@ import AuxFuncs as aux
 
 
 
-def main(): 
+def main():
 
     ### Initialize Log File ###
     tbOffset = len(tb.extract_stack())-1
@@ -63,16 +75,14 @@ def main():
     log.log("DetailsManager.py")
 
     start = datetime.now()
-    
-    
+
     ### Set Basic Parameters ###
     runNum = 3
     runsDir = "./Runs/"
     workDir = WORKING_DIR
-    
-    
+
     ### Run Conversion ###
-    convertBHDetails(runNum, runsDir, workDir, verbose=verbose)
+    convertBHDetails(runNum, runsDir, workDir, log=log)
 
     stop = datetime.now()
     log.log("Done after %s\n\n" % (str(stop-start)))
@@ -81,20 +91,26 @@ def main():
 
 
 
+###  ==================================================  ###
+###  =============  DETAILS FILE MANAGEMENT  ==========  ###
+###  ==================================================  ###
 
+
+
+'''
 def convertBHDetails(runNum, runsDir, workDir, log=None):
-    '''
+    """
     Move details information from illustris files to new ones organized by time.
 
-    '''
+    """
 
     if( log ): log.log("convertBHDetails()")
 
     ### Initialize Variables ###
 
-    # Get file names for pure Illustris Details files
+    # Get file names for Illustris BH Details files
     if( log ): log.log("Loading details filenames")
-    illDetFilenames = aux.getIllustrisBHDetailsFilenames(runNum, runsDir, log=log)
+    illDetFilenames = aux.getFilenames_Illustris_BHDetails(runNum)
     numIllDetFiles = len(illDetFilenames)
     if( log ): log.log("Loaded %d details filenames" % (numIllDetFiles) )
 
@@ -122,7 +138,7 @@ def convertBHDetails(runNum, runsDir, workDir, log=None):
 
     ### Organize Details by Snapshot Time; create new ASCII Files ###
     if( log ): log.log("Reorganizing by time")
-    reorganizeBHDetails(illDetFilenames, newDetASCIIFilenames, times, verbose=vb)
+    reorganizeBHDetails(illDetFilenames, newDetASCIIFilenames, times, log=log)
 
 
     ### Convert New Details ASCII Files, to new Details object files ###
@@ -130,9 +146,9 @@ def convertBHDetails(runNum, runsDir, workDir, log=None):
     convertDetailsASCIItoObj(newDetASCIIFilenames, newDetObjFilenames, log=log)
 
     return
+'''
 
-
-
+'''
 def reorganizeBHDetails(oldFilenames, newFilenames, times, log=None):
 
     if( log ): log.log("reorganizeBHDetails()")
@@ -184,36 +200,35 @@ def reorganizeBHDetails(oldFilenames, newFilenames, times, log=None):
     if( log ): log.log("Closed new files.  Average size %s" % (sizeStr) )
 
     return
+'''
 
 
 
-
-def convertDetailsASCIItoObj(ascFilenames, objFilenames, log=None):
-
-    if( log ): log.log("convertDetailsASCIItoObj()")
-    numFiles = len(ascFilenames)
+def convert_ASCII_to_NPZ(run):
 
     start = datetime.now()
-    startOne = datetime.now()
-    for ii, [ascName,objName] in enumerate( zip(ascFilenames,objFilenames) ):
+    last = start
 
-        details = loadIllustrisBHDetails( ascName, log=log)
-        saveBHDetails(details, objName, log=log)
+    for snap in range(NUM_SNAPS):
 
-        # Print where we are, and duration
-        now = datetime.now()
-        dur = str(now-start)
-        durOne = str(now-startOne)
-        if( log ): log.log("%d/%d after %s/%s" % (ii, numFiles, durOne, dur) )
-        startOne = now
+        ascName = getFilename_BHDetails_ASCII(run, snap)
+        npzName = getFilename_BHDetails_NPZ(run, snap)
 
-    # } ii
+        # Load ASCII File
+        detList = loadBHDetails_ASCII( ascName )
+        # Save NPZ File
+        saveBHDetails_NPZ(detList, npzName)
+
+        stop = datetime.now()
+        print "%d / %d  After  %s / %s" % (snap, NUM_SNAPS, str(stop-last), str(stop-start))
+        last = stop
+
 
     return
 
 
-'''
-def loadIllustrisBHDetails(fileName, log=None):
+
+def loadBHDetails_ASCII(fileName, log=None):
 
     if( log ): log.log("loadIllustrisBHDetails()")
 
@@ -234,19 +249,11 @@ def loadIllustrisBHDetails(fileName, log=None):
     # Trim excess
     del tmpList[num:]
 
-    ### Fill merger object with Merger Data ###
-    if( log ): log.log("Creating details object")
-    details = Details( num )
-    for ii, tmp in enumerate(tmpList):
-        details[ii] = tmp
-
-    return details
-'''
-
+    return tmpList
 
 
 def parseIllustrisBHDetailsLine(instr):
-    '''
+    """
     Parse a line from an Illustris blachole_details_#.txt file
 
     The line is formatted (in C) as:
@@ -254,7 +261,7 @@ def parseIllustrisBHDetailsLine(instr):
         (long long) P[n].ID, All.Time, BPP(n).BH_Mass, mdot, rho, soundspeed
 
     return ID, time, mass, mdot, rho, cs
-    '''
+    """
     args = instr.split()
 
     # First element is 'BH=########', trim to just the id number
@@ -263,71 +270,177 @@ def parseIllustrisBHDetailsLine(instr):
     return LONG(args[0]), DBL(args[1]), DBL(args[2]), DBL(args[3]), DBL(args[4]), DBL(args[5])
 
 
+def loadBHDetails_NPZ(run, snap):
+    detsName = getFilename_BHDetails_NPZ(run,snap)
+    dat = np.load(detsName)
+    return dat
 
 
-def loadBHDetails(runNum, snapNum, workDir, log=None):
-
-    if( log ): log.log("loadBHDetails()")
-
-    detsFilename = getBHDetailsObjFilename(runNum, snapNum, workDir)
-    if( log ): log.log('File %s' % (detsFilename) )
-    if( not os.path.exists(detsFilename) ):
-        raise RuntimeError("NO file %s!" % (detsFilename) )
-
-    dets = loadBHDetailsFromSave(detsFilename)
-    if( log ): log.log('Loaded %d details' % (len(dets)) )
-
-    return dets
-
-
-
-
-def saveBHDetails(details, saveFilename, log=None):
-    '''
+def saveBHDetails_NPZ(dets, saveFilename, log=None):
+    """
     Save details object using pickle.
 
     Overwrites any existing file.  If directories along the path don't exist,
     they are created.
-    '''
-
-    if( log ): log.log("saveBHDetails()")
+    """
 
     # Make sure output directory exists
     saveDir, saveName = os.path.split(saveFilename)
     aux.checkDir(saveDir)
 
-    # Save binary pickle file
-    if( log ): log.log("Saving details to '%s'" % (saveFilename) )
-    saveFile = open(saveFilename, 'wb')
-    pickle.dump(details, saveFile)
-    saveFile.close()
-    if( log ): log.log("Saved, size %s" % getFileSizeString(saveFilename) )
+    ### Convert From List of Lists to Arrays ###
+    nums = len(dets)
+    ids = np.zeros(nums, dtype=LONG)
+    scale = np.zeros(nums, dtype=DBL)
+    mass = np.zeros(nums, dtype=DBL)
+    mdot = np.zeros(nums, dtype=DBL)
+    rho = np.zeros(nums, dtype=DBL)
+    cs = np.zeros(nums, dtype=DBL)
+
+    for ii, row in enumerate(dets):
+        ids[ii]   = row[0]
+        scale[ii] = row[1]
+        mass[ii]  = row[2]
+        mdot[ii]  = row[3]
+        rho[ii]   = row[4]
+        cs[ii]    = row[5]
+
+
+    ### Create Dictionary and save to NPZ ###
+    dat = { DET_ID    : ids,
+            DET_SCALE : scale,
+            DET_MASS  : mass,
+            DET_MDOT  : mdot,
+            DET_RHO   : rho,
+            DET_CS    : cs }
+
+    np.savez(saveFilename, **dat)
+
+    print "\tSaved to '%s'" % (saveFilename)
+    print "\tSaved size = %s" % (aux.getFileSizeString(saveFilename))
 
     return
 
 
 
-def loadBHDetailsFromSave(loadFilename):
-    '''
-    Load details object from file.
-    '''
-    loadFile = open(loadFilename, 'rb')
-    details = pickle.load(loadFile)
-    return details
+###  =====================================================  ###
+###  =============  BH / DETAILS MATCHING  ===============  ###
+###  =====================================================  ###
 
 
-def getBHDetailsASCIIFilename(runNum, snapNum, workDir):
-    detsDir = workDir + (BH_DETAILS_DIR % (runNum))
+
+def detailsForBH(bhid, snapDets, side=None, log=None):
+    """
+    Retrieve the details entry for a particular BH at a target snapshot.
+
+
+    Parameters
+    ----------
+    bhid : int
+        ID of the target BH
+    snapDets :
+    side : {'left', 'right', default=None}, optional
+        Which matching elements to return.
+        None : return all matches
+        'left' : return the earliest match
+        'right' : return the latest match
+    log : class ObjLog, optional
+
+
+    Returns
+    -------
+    scale, mass, mdot, rho, cs : 5 scalars or scalar arrays with the matching
+                                 entries, determined by the `side` argument
+        scale : the scale factor of the entry
+        mass : the mass of the BH in each entry
+        mdot : the accretion rate of the BH in each entry
+        rho : the ambient density
+        cs : the ambient sound speed
+
+        All elements are set to `None` if no matches are found.
+
+    Raises
+    ------
+    RuntimeError
+        when the `side` argument is invalid
+
+    """
+
+    if( log ): log.log("detailsForBH()")
+    retErr = [None]*5
+
+    '''
+    ### Load Details for this Snapshot ###
+    snapDets = loadBHDetails_NPZ(run, snap)
+    '''
+
+    ### Find the Details index which matched ID and Nearest in Time ###
+
+    # Find details indices with BH ID match
+    inds = np.where( bhid == snapDets[DET_ID] )[0]
+
+    # If there are no matches, return None
+    if( len(inds) == 0 ):
+        if( log ): log.log("No matches in snap %d for ID %d" % (snap, bhid) )
+        return retErr
+
+    # Get times for matching details
+    detTimes = snapDets[DET_SCALE][inds]
+    # Get indices to sort times
+    sortInds = np.argsort(detTimes)
+
+
+    ### Determine Which Matching Entries to Return ###
+
+    # Return all matching entries
+    if( side == None ):
+        retInds = sortInds
+    # Return entry for earliest matching time
+    elif( side == 'left' ):
+        retInds = sortInds[0]
+    # Return entry for latest matching time
+    elif( side == 'right' ):
+        retInds = sortInds[-1]
+    # Error
+    else:
+        raise RuntimeError("Unrecognized side='%s'!" % (side) )
+
+
+    ### Return Matching Details ###
+
+    # Convert indices to global array
+    retInds = inds[retInds]
+    # Create output arrays/value
+    scale = snapDets[DET_SCALE][retInds]
+    mass  = snapDets[DET_MASS ][retInds]
+    mdot  = snapDets[DET_MDOT ][retInds]
+    rho   = snapDets[DET_RHO  ][retInds]
+    cs    = snapDets[DET_CS   ][retInds]
+
+    return scale, mass, mdot, rho, cs
+
+
+
+
+
+###  ===================================================  ###
+###  =============  AUXILIARY FUNCTIONS  ===============  ###
+###  ===================================================  ###
+
+
+
+def getFilename_BHDetails_ASCII(runNum, snapNum):
+    detsDir = (PP_BH_DETAILS_DIR % (runNum))
     aux.checkDir(detsDir)                                                                           # Make sure directory exists
-    asciiFilename = detsDir + (BH_DETAILS_ASCII_FILENAME % (runNum, snapNum))
+    asciiFilename = detsDir + (PP_BH_DETAILS_ASCII_FILENAME % (runNum, snapNum))
     return asciiFilename
 
 
-def getBHDetailsObjFilename(runNum, snapNum, workDir):
-    detsDir = workDir + (BH_DETAILS_DIR % (runNum))
+def getFilename_BHDetails_NPZ(runNum, snapNum):
+    detsDir = (PP_BH_DETAILS_DIR % (runNum))
     aux.checkDir(detsDir)                                                                           # Make sure directory exists
-    asciiFilename = detsDir + (BH_DETAILS_OBJ_FILENAME % (runNum, snapNum))
-    return asciiFilename
+    objFilename = detsDir + (PP_BH_DETAILS_NPZ_FILENAME % (runNum, snapNum))
+    return objFilename
 
 
 
