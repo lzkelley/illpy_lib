be9493e5d1c049c52ae82ae0c2423cbcdebf10a5
lzkelley <lkelley@cfa.harvard.edu>
2014-12-08 00:27:11 -0500

    Cleanup

--- FindRepeats.pyx
+++ FindRepeats.pyx
@@ -36,19 +36,8 @@ def findRepeats(np.ndarray[long, ndim=2] ids, np.ndarray[float, ndim=1] times):
 
     """
     
-    
     cdef long numMergers = ids.shape[0]
-
     cdef int hunth = np.int(np.floor(numMergers*0.01))
-
-    '''
-    cdef float aveInter = 0.0
-    cdef float aveFuture = 0.0
-    cdef float avePast = 0.0
-    cdef long aveInterNum = 0
-    cdef long aveFutureNum = 0
-    cdef long avePastNum = 0
-    '''
 
     cdef long out, ss, ii, jj
 
@@ -59,8 +48,6 @@ def findRepeats(np.ndarray[long, ndim=2] ids, np.ndarray[float, ndim=1] times):
     cdef np.ndarray next  = -1*np.ones(numMergers, dtype=long)                                      # Map to the next merger
     cdef np.ndarray last  = -1*np.ones(numMergers, dtype=long)                                      # Map to previous merger
     cdef np.ndarray inter = -1.0*np.ones(numMergers, dtype=float)                                  # Time to next merger
-    #cdef np.ndarray numFuture = -1*np.ones(numMergers, dtype=long)
-    #cdef np.ndarray numPast = -1*np.ones(numMergers, dtype=long)
 
 
     ### Iterate Over Each Merger, In Order of Merger Time ###
@@ -92,59 +79,11 @@ def findRepeats(np.ndarray[long, ndim=2] ids, np.ndarray[float, ndim=1] times):
                 # Set time between mergers
                 inter[ss] = times[tt] - times[ss]
 
-                # Cumulate times to get average
-                # aveInter += inter[ss]
-                # aveInterNum += 1
-                
+
         # } jj
 
     # } ii
 
-
-    # Calculate average
-    # if( aveInterNum > 0 ): aveInter /= aveInterNum
-
-    '''
-    ### Go back through All Mergers to Count Repeats ###
-    
-    for ii in xrange(numMergers):
-
-        ## Count Forward from First Mergers ##
-        # If this is a first merger
-        if( last[ii] < 0 ):
-            jj = next[ii]
-            # Walk through future mergers
-            while( jj >= 0 ):
-                numFuture[ii] += 1
-                jj = next[jj]
-
-            # Accumulate for averaging
-            aveFuture += numFuture[ii]
-            aveFutureNum += 1
-
-                
-        ## Count Backward from Last Mergers ##
-        # If this is a final merger
-        if( next[ii] < 0 ):
-            jj = last[ii]
-            # Walk through past mergers
-            while( jj >= 0 ):
-                numPast[ii] += 1
-                jj = last[jj]
-
-            # Accumulate for averaging
-            avePast += numPast[ii]
-            avePastNum += 1
-
-
-    # } ii
-
-    # Calculate averages
-    if( avePastNum   > 0 ): avePast   /= avePastNum
-    if( aveFutureNum > 0 ): aveFuture /= aveFutureNum
-    '''
-
-    # return inter, aveInter, numFuture, aveFuture, numPast, avePast
     return inter, next, last
 
 
