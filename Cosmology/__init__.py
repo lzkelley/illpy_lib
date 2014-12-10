cacc078ea5f0eda276b666662adfc40518fbe753
lzkelley <lkelley@cfa.harvard.edu>
2014-12-10 14:06:31 -0500

    Restructured Cosmology module

---
+++ Cosmology/__init__.py
@@ -0,0 +1,8 @@
+"""
+Handle Cosmological parameters from Illustris Simulations.
+"""
+
+__author__ = "Luke Zoltan Kelley"
+__version__ = "1.0"
+__email__ = "lkelley@cfa.harvard.edu"
+__status__ = "Development"
