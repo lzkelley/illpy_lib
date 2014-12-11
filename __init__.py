37837b65a1986b19757556d6361db1bea6fe9aed
lzkelley <lkelley@cfa.harvard.edu>
2014-12-11 15:45:31 -0500

    __init__.py : added Physics and AuxFuncs to initial imports

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 """
 
 __author__ = "Luke Zoltan Kelley"
-__version__ = "1.0"
+__version__ = "0.1"
 __email__ = "lkelley@cfa.harvard.edu"
 __status__ = "Development"
 
 from Cosmology import Cosmology
 import Constants
+import Physics
+import AuxFuncs
