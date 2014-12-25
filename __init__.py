3e269a7f5119e832012fd9425a924619f332d0b6
lzkelley <lkelley@cfa.harvard.edu>
2014-12-25 01:06:44 -0500

    __init__.py : updated for new Cosmology ==> illcosmo name

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ __version__ = "0.1"
 __email__ = "lkelley@cfa.harvard.edu"
 __status__ = "Development"
 
-from Cosmology import Cosmology
+import illcosmo
 import Constants
 import Physics
 import AuxFuncs
