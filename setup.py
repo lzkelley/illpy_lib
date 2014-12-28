7bdd8f78ddfa7516ead0ecb06e5117ca0455ed77
lzkelley <lkelley@cfa.harvard.edu>
2014-12-28 18:49:37 -0500

    setup.py script for 'illpy'

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+readme = open('README').read()
+
+requirements = [
+
+]
+
+setup(
+    name = "illpy",
+    version = "0.0.1",
+    author = "Luke Zoltan Kelley",
+    author_email = "lkelley@cfa.harvard.edu",
+    description = ("General functions for dealing with the illustris simulations."),
+    license = "MIT",
+    keywords = "",
+    url = "https://bitbucket.org/lzkelley/illpy",
+    packages=['illpy'],
+    include_package_data=True,
+    install_requires=requirements,
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
