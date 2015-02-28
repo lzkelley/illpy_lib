6b5373518ee4812a63da6be6027862d9ddcb2b88
lzkelley <lkelley@cfa.harvard.edu>
2015-02-28 14:33:36 -0500

    Added BHDetails constants, and an '__all__' to specificy imports

--- illpy/illbh/BHConstants.py
+++ illpy/illbh/BHConstants.py
@@ -2,12 +2,16 @@
 import os
 import numpy as np
 
+__all__ = [ MERGERS_TIMES, MERGERS_IDS, MERGERS_MASSES, MERGERS_DIR, MERGERS_RUN, MERGERS_NUM, \
+            MERGERS_CREATED, MERGERS_MAP_STOM, MERGERS_MAP_MTOS, MERGERS_MAP_ONTOP, \
+            IN_BH, OUT_BH, DATA_PATH, _DOUBLE, _LONG, \
+            DETAIL_IDS, DETAIL_TIMES, DETAIL_MASSES, DETAIL_MDOTS, DETAIL_RHOS, DETAIL_CS, \
+            DETAIL_RUN, DETAIL_SNAP, DETAIL_NUM, DETAIL_CREATED, DETAIL_BEFORE, DETAIL_AFTER ]
+
 DATA_PATH = "%s/data/" % os.path.dirname(os.path.abspath(__file__))
 
 _DOUBLE = np.float64
 _LONG = np.int64
-
-
 
 
 # Key Names for Mergers Dictionary
@@ -25,3 +29,19 @@ MERGERS_MAP_ONTOP = 'ontop'
 # Index of [N,2] arrays corresponding to each BH
 IN_BH  = 0
 OUT_BH = 1
+
+
+### Dictionary Keys for Details Parameters ###
+DETAIL_IDS     = 'id'
+DETAIL_TIMES   = 'times'
+DETAIL_MASSES  = 'masses'
+DETAIL_MDOTS   = 'mdots'
+DETAIL_RHOS    = 'rhos'
+DETAIL_CS      = 'cs'
+DETAIL_RUN     = 'run'
+DETAIL_SNAP    = 'snap'
+DETAIL_NUM     = 'num'
+DETAIL_CREATED = 'created'
+
+DETAIL_BEFORE  = 0                                                                                  # Before merger time (MUST = 0!)
+DETAIL_AFTER   = 1                                                                                  # After (or equal) merger time (MUST = 1!)
