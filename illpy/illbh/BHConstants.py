870bb0f10cf415b1c97af26c1634c343306058d1
lzkelley <lkelley@cfa.harvard.edu>
2015-02-28 22:07:58 -0500

    Fixed error with '__all__'

--- illpy/illbh/BHConstants.py
+++ illpy/illbh/BHConstants.py
@@ -2,11 +2,12 @@
 import os
 import numpy as np
 
-__all__ = [ MERGERS_TIMES, MERGERS_IDS, MERGERS_MASSES, MERGERS_DIR, MERGERS_RUN, MERGERS_NUM, \
-            MERGERS_CREATED, MERGERS_MAP_STOM, MERGERS_MAP_MTOS, MERGERS_MAP_ONTOP, \
-            IN_BH, OUT_BH, DATA_PATH, _DOUBLE, _LONG, \
-            DETAIL_IDS, DETAIL_TIMES, DETAIL_MASSES, DETAIL_MDOTS, DETAIL_RHOS, DETAIL_CS, \
-            DETAIL_RUN, DETAIL_SNAP, DETAIL_NUM, DETAIL_CREATED, DETAIL_BEFORE, DETAIL_AFTER ]
+__all__ = [ 'MERGERS_TIMES', 'MERGERS_IDS', 'MERGERS_MASSES', 'MERGERS_DIR', 'MERGERS_RUN', 'MERGERS_NUM', \
+            'MERGERS_CREATED', 'MERGERS_MAP_STOM', 'MERGERS_MAP_MTOS', 'MERGERS_MAP_ONTOP', \
+            'IN_BH', 'OUT_BH', 'DATA_PATH', '_DOUBLE', '_LONG', \
+            'DETAIL_IDS', 'DETAIL_TIMES', 'DETAIL_MASSES', 'DETAIL_MDOTS', 'DETAIL_RHOS', 'DETAIL_CS', \
+            'DETAIL_RUN', 'DETAIL_SNAP', 'DETAIL_NUM', 'DETAIL_CREATED', 'DETAIL_BEFORE', 'DETAIL_AFTER', \
+            'DETAIL_PHYSICAL_KEYS' ]
 
 DATA_PATH = "%s/data/" % os.path.dirname(os.path.abspath(__file__))
 
@@ -45,3 +46,5 @@ DETAIL_CREATED = 'created'
 
 DETAIL_BEFORE  = 0                                                                                  # Before merger time (MUST = 0!)
 DETAIL_AFTER   = 1                                                                                  # After (or equal) merger time (MUST = 1!)
+
+DETAIL_PHYSICAL_KEYS = [ DETAIL_IDS, DETAIL_TIMES, DETAIL_MASSES, DETAIL_MDOTS, DETAIL_RHOS, DETAIL_CS ]
