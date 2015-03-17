db1ef07d30ba108eba01bb56d938fa33ec108c37
lzkelley <lkelley@cfa.harvard.edu>
2015-03-17 13:08:50 -0400

    Fixed bug where index used to store results wasn't the correct one. Changed variable names for clarity.

--- FindRepeats.pyx
+++ FindRepeats.pyx
@@ -30,12 +30,12 @@ def findRepeats(np.ndarray[long,   ndim=2] ids,   np.ndarray[double, ndim=1] tim
 
     """
 
-    cdef long outid, ss, ii, jj, tt
+    cdef long outid, ii, jj, next_ind, last_ind
     cdef long numMergers = ids.shape[0]
     cdef int hunth = np.int(np.floor(0.01*numMergers))
 
     # Get indices to sort mergers by time
-    cdef np.ndarray sort_t = np.argsort(times)
+    cdef np.ndarray sort_inds = np.argsort(times)
 
     ### Iterate Over Each Merger, In Order of Merger Time ###
 
@@ -44,33 +44,35 @@ def findRepeats(np.ndarray[long,   ndim=2] ids,   np.ndarray[double, ndim=1] tim
         if( ii > 0 and ii%hunth == 0 ): print "%5d/%d" % (ii, numMergers)
 
         # Conver to sorted merger index
-        mind = sort_t[ii]
+        last_ind = sort_inds[ii]
 
         # Get the output ID from this merger
-        outid = ids[mind, OUT_BH]
-
+        outid = ids[last_ind, OUT_BH]
 
         ## Iterate over all Later Mergers ##
         #  use a while loop so we can break out of it
         jj = ii+1
         while( jj < numMergers ):
             # Convert to sorted index
-            tt = sort_t[jj]
+            next_ind = sort_inds[jj]
 
             # If previous merger goes into this one; save relationships
             for BH in [IN_BH, OUT_BH]:
 
-                if( ids[tt,BH] == outid ):
-                    # Set index of next merger
-                    next[ss] = tt
+                if( ids[next_ind,BH] == outid ):
+
                     # Set index of last merger
-                    last[tt,BH] = ss
+                    last[next_ind,BH] = last_ind
+                    # Set index of next merger
+                    next[last_ind] = next_ind
                     # Set time between mergers
-                    inter[ss,BH] = times[tt] - times[ss]
+                    inter[last_ind,BH] = times[next_ind] - times[last_ind]
 
                     # Break back to highest for-loop over all mergers (ii)
                     jj = numMergers
                     break
+
+
 
             # Increment
             jj += 1
@@ -80,8 +82,6 @@ def findRepeats(np.ndarray[long,   ndim=2] ids,   np.ndarray[double, ndim=1] tim
     # } ii
 
     return 
-    #return last, next, inter
-
 
 
 
