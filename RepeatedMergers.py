06a237e22e9a8f71d9ff28ed4008c7786045f1d5
lzkelley <lkelley@cfa.harvard.edu>
2015-03-17 13:10:09 -0400

    Added method 'getRepeats()' to handle loading or rederiving repeat data. Working on revamping the analye repeats function aswell

--- RepeatedMergers.py
+++ RepeatedMergers.py
@@ -33,6 +33,7 @@ import FindRepeats
 import plotting as gwplot
 
 RUN = 3                                                                                         # Which illustris simulation to target
+VERBOSE = True
 FILE_NAME = lambda xx: "ill-%d_repeated-mergers.npz" % (xx)
 
 LOAD = False
@@ -44,13 +45,15 @@ REPEAT_CREATED = 'created'
 REPEAT_RUN = 'run'
 
 
+HEADER = " - RepeatedMergers :"
+
 ###  ==============================================================  ###
 ###  ===========================  MAIN  ===========================  ###
 ###  ==============================================================  ###
 
 
 
-def main(run=RUN, load=LOAD):
+def main(run=RUN, load=LOAD, verbose=VERBOSE):
 
     ### Initialize Log File ###
     print "\nRepeatedMergers.py\n"
@@ -65,32 +68,11 @@ def main(run=RUN, load=LOAD):
     print " - - Loaded after %s" % (str(stop-start))
 
 
-    ### Find Repeated Mergers ###
-
-    fname = FILE_NAME(run)
-    # Try to load precalculated repeat data
-    if( os.path.exists(fname) and not load ): 
-        print " - Loading Repeated Merger Data"
-        start = datetime.now()
-        dat = np.load(fname)
-        interval = dat['inter']
-        next = dat['next']
-        last = dat['last']
-        stop = datetime.now()
-        print " - - Loaded after %s" % (str(stop-start))
-
-    # Reload repeat data from mergers
-    else:
-        print " - Finding Repeated Mergers"
-        start = datetime.now()
-        repeats = repeatedMergerTimes(run, base)
-        aux.saveDictNPZ(repeats, fname, verbose=True)
-        stop = datetime.now()
-        print " - - Done after %s" % (str(stop-start))
-
+    ### Load Repeated Mergers ###
+    repeats = getRepeats(run, base, load=False, verbose=verbose)
 
     ### Process Repeat Data ###
-    lowerInter, numPast, numFuture = processRepeats(interval, next, last, base)
+    lowerInter, numPast, numFuture = analyzeRepeats(interval, next, last, base)
 
     return interval, lowerInter, numPast, numFuture
 
@@ -109,7 +91,52 @@ def main(run=RUN, load=LOAD):
 
 
 
+def getRepeats(run, base, load=False, verbose=VERBOSE):
+    """
+    Load repeat data from save file if possible, or recalculate directly.
 
+    Arguments
+    ---------
+    run : <int>
+        Illlustris run number {1,3}
+    base : <Basics>, Basics.py:Basics object
+        Contains merger information
+    load : <bool>, (optional=False)
+        Reload repeat data directly from merger data
+    verbose : <bool>, (optional=VERBOSE)
+        Print verbose output
+
+    Returns
+    -------
+    repeats : <dict>
+        container for repeat data - see RepeatedMergers doc
+
+    """
+
+    if( verbose ): print " - - RepeatedMergers.getRepeats()"
+
+    fname = FILE_NAME(run)
+    # Try to load precalculated repeat data
+    if( os.path.exists(fname) and not load ): 
+        if( verbose ) : print " - - - Loading Repeated Merger Data from '%s'" % (fname)
+        start = datetime.now()
+        repeats = np.load(fname)
+        stop = datetime.now()
+        if( verbose ) : print " - - - - Loaded after %s" % (str(stop-start))
+
+    # Reload repeat data from mergers
+    else:
+        print " - - - Finding Repeated Mergers from Merger Data"
+        start = datetime.now()
+        # Find Repeats
+        repeats = calculateRepeatedMergers(run, base)
+        # Save Repeats data
+        aux.saveDictNPZ(repeats, fname, verbose=True)
+        stop = datetime.now()
+        if( verbose ): print " - - - - Done after %s" % (str(stop-start))
+
+
+    return repeats
 
 
 ###  ==============================================================  ###
@@ -118,9 +145,27 @@ def main(run=RUN, load=LOAD):
 
 
 
-def repeatedMergerTimes(run, base):
+def calculateRepeatedMergers(run, base, verbose=VERBOSE):
+    """
+    Use merger data to find and connect BHs which merge multiple times.
 
-    print " - repeatedMergerTimes()"
+    Arguments
+    ---------
+    run : <int>
+        Illlustris run number {1,3}
+    base : <Basics>, Basics.py:Basics object
+        Contains merger information
+    verbose : <bool>, (optional=VERBOSE)
+        Print verbose output
+
+    Returns
+    -------
+    repeats : <dict>
+        container for repeat data - see RepeatedMergers doc
+
+    """
+
+    if( verbose ): print " - - RepeatedMergers.calculateRepeatedMergers()"
 
     numMergers = base.mergers[MERGERS_NUM]
     next  = -1*  np.ones([numMergers],              dtype=long)
@@ -128,40 +173,63 @@ def repeatedMergerTimes(run, base):
     inter = -1.0*np.ones([numMergers,NUM_BH_TYPES], dtype=np.float64)
 
     # Convert merger scale factors to ages
-    print " - - Converting merger times"
+    if( verbose ): print " - - - Converting merger times"
     start = datetime.now()
     scales = base.mergers[MERGERS_TIMES]
     times = np.array([ base.cosmo.age(sc) for sc in scales ], dtype=np.float64)
     stop = datetime.now()
-    print " - - - Done after %s" % (str(stop-start))
+    if( verbose ): print " - - - - Done after %s" % (str(stop-start))
 
 
     # Get repeated merger information
-    print " - - Getting repeat statistics"
+    if( verbose ): print " - - - Getting repeat statistics"
     start = datetime.now()
     mids = base.mergers[MERGERS_IDS]
     FindRepeats.findRepeats(mids, times, last, next, inter)
     stop = datetime.now()
-    print " - - - Retrieved after %s" % (str(stop-start))
+    if( verbose ): print " - - - - Retrieved after %s" % (str(stop-start))
+
+    inds = np.where( last < 0 )[0]
+    print "MISSING LAST = ", len(inds)
+
+    inds = np.where( next < 0 )[0]
+    print "MISSING NEXT = ", len(inds)
+
 
     # Create dictionary to store data
-    repeatDict = { REPEAT_LAST     : last,
-                   REPEAT_NEXT     : next,
-                   REPEAT_INTERVAL : inter,
-                   REPEAT_CREATED  : datetime.now().ctime(),
-                   REPEAT_RUN      : run }
+    repeats = { REPEAT_LAST     : last,
+                REPEAT_NEXT     : next,
+                REPEAT_INTERVAL : inter,
+                REPEAT_CREATED  : datetime.now().ctime(),
+                REPEAT_RUN      : run }
 
-    return repeatDict
-
-
+    return repeats
 
 
 
-def processRepeats(repeats, base):
 
-    print " - processRepeats()"
+def analyzeRepeats(repeats, base, verbose=VERBOSE):
+    """
+    Analyze the data from calculation of repeated mergers to obtain typical number of repeats, etc.
+
+    Arguments
+    ---------
+    repeats : <dict>
+        container for repeat data - see RepeatedMergers doc
+    base : <Basics>, Basics.py:Basics object
+        Contains merger information
+    verbose : <bool>, (optional=VERBOSE)
+        Print verbose output
+
+    Returns
+    -------
+
+
+    """
+
+    if( verbose ): print " - - RepeatedMergers.analyzeRepeats()"
     
-    print " - - %d Mergers" % (base.numMergers)
+    numMergers = base.numMergers
 
     interval = repeats[REPEAT_INTERVAL]
     last     = repeats[REPEAT_LAST]
@@ -177,7 +245,36 @@ def processRepeats(repeats, base):
     numFuture = -1*np.ones(base.numMergers, dtype=int)
 
     # Get age of the universe now
-    nowTime = base.cosmo.age(1.0) #0.999999)
+    nowTime = base.cosmo.age(1.0)
+
+
+
+    if( verbose ): print " - - - %d Mergers" % (numMergers)
+
+    # Find number of unique merger BHs (i.e. no previous mergers)
+    inds = np.where( (last[:,IN_BH] < 0) & (last[:,OUT_BH] < 0) & (next[:] < 0) )
+    numTwoIsolated = len(inds)
+    inds = np.where( ((last[:,IN_BH] < 0) ^ (last[:,OUT_BH] < 0)) & (next[:] < 0) )                 # 'xor' comparison
+    numOneIsolated = len(inds)
+    
+    if( verbose ): 
+        print " - - - Mergers with neither BH previously merged = %d" % (numTwoIsolated)
+        print " - - - Mergers with one     BH previously merged = %d" % (numOneIsolated)
+
+
+    inds = np.where( interval == 0.0 )
+    print "%d Intervals = 0.0" % (len(inds[0]))
+    if( len(inds[0]) > 0 ):
+        print "\te.g."
+        print "\t", inds[0][0], inds[0][1], " : ", interval[inds]
+
+
+    inds = np.where( interval < 0.0 )
+    print "%d Intervals < 0.0" % (len(inds[0]))
+    if( len(inds[0]) > 0 ):
+        print "\te.g."
+        print "\t", inds[0][0], inds[0][1], " : ", interval[inds]
+
 
     ### Go back through All Mergers to Count Repeats ###
     
