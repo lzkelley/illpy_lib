2c2bab8e92314b7dfa76146f681f2022095e9213
lzkelley <lkelley@cfa.harvard.edu>
2015-03-18 11:51:10 -0400

    Minor cleanup

--- illpy/illbh/setup.py
+++ illpy/illbh/setup.py
@@ -2,6 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    #name = 'FilterData',
-    ext_modules = cythonize("*.pyx"), #cythonize("FilterData.pyx"),
+    ext_modules = cythonize("*.pyx"),
     )
