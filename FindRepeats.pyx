0adc7f60a9b00514f16befc1d61171d534c9066c
lzkelley <lkelley@cfa.harvard.edu>
2015-03-18 10:43:51 -0400

    Added separate arrays to store time since previous merger and time to next merger

--- FindRepeats.pyx
+++ FindRepeats.pyx
@@ -1,6 +1,5 @@
 """
 
-
 """
 
 
@@ -10,23 +9,44 @@ cimport numpy as np
 import illpy
 from illpy.illbh.BHConstants import IN_BH, OUT_BH
 
-def findRepeats(np.ndarray[long,   ndim=2] ids,   np.ndarray[double, ndim=1] times,
-                np.ndarray[long,   ndim=2] last,  np.ndarray[long,   ndim=1] next,  
-                np.ndarray[double, ndim=2] inter ):
+def findRepeats(np.ndarray[long,   ndim=2] ids,      np.ndarray[double, ndim=1] times,
+                np.ndarray[long,   ndim=2] last,     np.ndarray[long,   ndim=1] next,  
+                np.ndarray[double, ndim=2] lastTime, np.ndarray[double, ndim=1] nextTime ):
     """
+    Given the complete list of merger IDs and Times, connect Mergers with the same BHs.
 
-    Parameters
-    ----------
-    ids : <long>[N,2]
-          array of merger BH indices (for both 'in' and 'out' BHs)
+    For each Merger system, take the 'out' BH and see if that BH participates in any future merger.
+    The output array ``next`` provides the index for the 'next' merger which the 'out' BH
+    participates in.  When such a 'repeat' is found, the second ('next') merger also has the
+    previous merger stored in the ``last`` array, where `last[ii,jj]` says that BH `jj`
+    ({IN_BH,OUT_BH}) from merger `ii` was previously the 'out' BH from merger `last[ii,jj]`.
 
-    times : <double>[N]
-            array of merger times -- !! in units of age of the universe !!
+    The array `lastTime[ii,jj]` gives the time from merger `ii` *since the previous merger* which
+    each BH `jj` ({IN_BH,OUT_BH}) participated in.  `nextTime[ii]` gives the time until the 'out'
+    BH from merger `ii` mergers mergers again.
 
-    Notes
-    -----
-    - This does not account for any mergers for which *both* entering BHs were
-      previous mergers.
+    All array entries (mergers) without repeat matches maintain their default value --- which
+    should be something like `-1`.
+
+    Arguments
+    ---------
+    ids : IN, <long>[N,2]
+        array of merger BH indices (for both 'in' and 'out' BHs)
+
+    times : IN, <double>[N]
+        array of merger times -- !! in units of age of the universe !!
+
+    last : INOUT, <long>[N,2]
+        Index of previous merger for each BH participating in the given merger.
+
+    next : INOUT, <long>[N]
+        Index of the next merger for the resulting 'out' BH.
+    
+    lastTime : INOUT, <double>[N,2]
+        Time *since the last* merger event, for each BH.
+
+    nextTime : INOUT, <double>[N]
+        Time *until the next* merger event for the 'out' BH.
 
     """
 
@@ -61,12 +81,18 @@ def findRepeats(np.ndarray[long,   ndim=2] ids,   np.ndarray[double, ndim=1] tim
 
                 if( ids[next_ind,BH] == outid ):
 
-                    # Set index of last merger
+                    ## For the 'next' Merger
+                    # set index of previous merger
                     last[next_ind,BH] = last_ind
-                    # Set index of next merger
+                    # set time since previous merger
+                    lastTime[next_ind,BH] = times[next_ind] - times[last_ind]
+
+                    ## For the 'previous' Merger
+                    # set index of next merger
                     next[last_ind] = next_ind
-                    # Set time between mergers
-                    inter[last_ind,BH] = times[next_ind] - times[last_ind]
+                    # set time until merger
+                    nextTime[next_ind] = times[next_ind] - times[last_ind]
+
 
                     # Break back to highest for-loop over all mergers (ii)
                     jj = numMergers
