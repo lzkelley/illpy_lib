e674a0e678daa6a94e2c4a49a29ccb9cb844a7a3
lzkelley <lkelley@cfa.harvard.edu>
2015-03-26 13:29:56 -0400

    Provide constants and hard-settings

---
+++ Subhalos/Constants.py
@@ -0,0 +1,15 @@
+"""
+Provide common constants and hard-settings.
+
+"""
+
+
+
+### SubLink Merger Trees ###
+
+ILLUSTRIS_TREE_PATH_BASE = '/n/ghernquist/Illustris/Runs/%s/trees/SubLink_gal'
+
+ILLUSTRIS_TREE_PATHS = { 1: ILLUSTRIS_TREE_PATH_BASE % 'L75n1820FP',
+                         2: ILLUSTRIS_TREE_PATH_BASE % 'L75n910FP' ,
+                         3: ILLUSTRIS_TREE_PATH_BASE % 'L75n455FP'   }
+
