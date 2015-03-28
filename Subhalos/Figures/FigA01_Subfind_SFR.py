b1e4bc9fe69debfd5b52e64c63e5676570b3a8f6
lzkelley <lkelley@cfa.harvard.edu>
2015-03-28 18:48:25 -0400

    Added histograms of counts of SFR and SSFR; also some lines for averages and percentiles

--- Subhalos/Figures/FigA01_Subfind_SFR.py
+++ Subhalos/Figures/FigA01_Subfind_SFR.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-import scipy as sp
 
 import PlotFuncs as pfunc
 import illpy
@@ -13,6 +12,7 @@ RIGHT    = 0.9
 BOTTOM   = 0.1
 TOP      = 0.95
 HSPACE   = 0.4
+WSPACE   = 0.4
 
 FS = 14
 FS_TITLE = 16
@@ -27,6 +27,8 @@ SCALE = 'log'
 DASHES = [8,4]
 DOTS = [3,2]
 
+NUM = 40
+PERCENTILES = [10,50,90]
 
 AXIS_BH      = 0
 AXIS_STELLAR = 1
@@ -49,22 +51,30 @@ def plotFigA01_Subfind_SFR(run, sfr, massTypes, massBH, fname=None, verbose=True
     massBH      = massBH*MASS_CONV
     massStellar = massTypes[:,PARTICLE_TYPE_STAR]*MASS_CONV
     massTotal   = np.sum(massTypes, axis=1)*MASS_CONV                                               # Sum over all particle types
+    nums        = len(sfr)
 
     # Calculate Specific SFR
     sfr_sp = sfr/massStellar
 
+    # Calculate statistics
+    stats    = np.percentile(sfr,    PERCENTILES)
+    stats_sp = np.percentile(sfr_sp, PERCENTILES)
 
     ### Create Figure and Axes ###
-    fig, ax = plt.subplots(figsize=FIG_SIZE, nrows=2, ncols=1, squeeze=False)
-    plt.subplots_adjust(left=LEFT, right=RIGHT, bottom=BOTTOM, hspace=HSPACE, top=TOP)
+    fig, ax = plt.subplots(figsize=FIG_SIZE, nrows=2, ncols=2, squeeze=False)
+    plt.subplots_adjust(left=LEFT, right=RIGHT, bottom=BOTTOM, hspace=HSPACE, top=TOP, wspace=WSPACE)
 
+    ## Plot Scatter SFR versus BH Mass
+    dots, names = figa01_scatter(ax[0,1], massBH, sfr, sfr_sp, which=AXIS_BH, stats=stats)
 
-    ## Plot versus BH Mass
-    dots, names = figa01_panel(ax[0,0], massBH, sfr, sfr_sp, which=AXIS_BH)
+    ## Plot Histogram SFR
+    figa01_hist(ax[0,0], sfr, specific=False, stats=stats)
 
-    ## Plot versus Stellar Mass
-    dots, names = figa01_panel(ax[1,0], massStellar, sfr, sfr_sp, which=AXIS_STELLAR)
+    ## Plot Scatter SFR versus Stellar Mass
+    dots, names = figa01_scatter(ax[1,0], massStellar, sfr, sfr_sp, which=AXIS_STELLAR, stats_sp=stats_sp)
 
+    ## Plot Histogram Specific SFR
+    figa01_hist(ax[1,1], sfr_sp, specific=True, stats=stats_sp)
 
 
     # Add Legend
@@ -80,26 +90,32 @@ def plotFigA01_Subfind_SFR(run, sfr, massTypes, massBH, fname=None, verbose=True
 
 
 
-def figa01_panel(ax, args, sfr, sfr_sp=None, which=AXIS_BH):
+def figa01_scatter(ax, args, sfr, sfr_sp=None, which=AXIS_BH, stats=None, stats_sp=None):
 
     assert which in AXIS_TYPES, "``which`` must be in '%s' !!" % (AXIS_TYPES)
 
+    dots = []
+    names = []
 
+    ### Configure Axes ###
     ylabel = r'Star Formation Rate [Msol/yr]'
 
     if(   which is AXIS_BH      ): xlabel = r'Blackhole Mass [$M_\odot$]'
     elif( which is AXIS_STELLAR ): xlabel = r'Stellar Mass [$M_\odot$]'
     else:                          xlabel = r'Total Mass [$M_\odot$]'
 
-    dots = []
-    names = []
-
     pfunc.setAxis(ax, axis='x', fs=FS, c='black', label=xlabel, scale=SCALE)
-    pfunc.setAxis(ax, axis='y', fs=FS, c='black', label=ylabel, scale=SCALE)
+    pfunc.setAxis(ax, axis='y', fs=FS, c=COL_SFR, label=ylabel, scale=SCALE)
+
+
+    ### Plot SFR ###
 
     s1 = ax.scatter(args, sfr, lw=LW, c=COL_SFR, marker='o', s=PS, alpha=ALPHA)
     dots.append(s1)
     names.append('SFR')
+    
+
+    ### Plot Specific SFR on Second Axis ###
 
     if( sfr_sp is not None ):
         y2label = r'Specific SFR [(Msol/yr)/Msol]'
@@ -108,6 +124,59 @@ def figa01_panel(ax, args, sfr, sfr_sp=None, which=AXIS_BH):
         dots.append(s2)
         names.append('Specific SFR')
 
+        if( stats_sp is not None ):
+            for st in stats_sp:
+                l1 = ax2.axhline(st, ls='-', lw=2*LW, color='0.5', alpha=0.5)
+                l1 = ax2.axhline(st, ls='--', lw=LW, color=COL_SFR_SP)
+                l1.set_dashes(DASHES)
 
+
+    if( stats is not None ):
+        for st in stats:
+            l1 = ax.axhline(st, ls='-', lw=2*LW, color='0.5')
+            l1 = ax.axhline(st, ls='--', lw=LW, color=COL_SFR)
+            l1.set_dashes(DASHES)
+
+
+
+    # Set xlimits to extrema
+    xlim = [np.min(args), np.max(args)]
+    ax.set_xlim(xlim)
 
     return dots, names
+
+
+
+
+def figa01_hist(ax, sfr, num=NUM, specific=False, stats=None):
+
+    # Specific --- lower right, use right-axis
+    if( specific ): 
+        ylabel = r'Specific SFR [(Msol/yr)/Msol]'
+        col = COL_SFR_SP
+        pos = 1.0
+    # Normal   --- upper left,  use left-axis
+    else:
+        ylabel = r'Star Formation Rate [Msol/yr]'
+        col = COL_SFR
+        pos = 0.0
+
+    # Setup Axes
+    pfunc.setAxis(ax, axis='x', fs=FS, c='black', label='Count', scale=SCALE)
+    pfunc.setAxis(ax, axis='y', fs=FS, c=col, label=ylabel, scale=SCALE, pos=pos)
+    if( not specific ): ax.invert_xaxis()
+
+    # Create Bins
+    bins = np.array([np.min(sfr[np.nonzero(sfr)]), np.max(sfr)])
+    bins = np.logspace( *np.log10(bins), num=num )
+
+    # Plot
+    ax.hist(sfr, bins=bins, orientation='horizontal', log=True, color=col)
+
+    if( stats is not None ):
+        for st in stats:
+            l1 = ax.axhline(st, ls='-', lw=2*LW, color='0.5')
+            l1 = ax.axhline(st, ls='--', lw=LW, color=col)
+            l1.set_dashes(DASHES)
+
+    return
