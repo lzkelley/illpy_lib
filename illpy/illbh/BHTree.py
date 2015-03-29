1852bd1e413363fe34bab68b7113b1745bbda60f
lzkelley <lkelley@cfa.harvard.edu>
2015-03-29 17:56:43 -0400

    Removed age calculations from here

--- illpy/illbh/BHTree.py
+++ illpy/illbh/BHTree.py
@@ -23,11 +23,6 @@ MYR = (1.0e6)*YEAR
 _TREE_SAVE_FILENAME = "ill-%d_bh-tree_v%.1f.npz"
 bhTree_filename = lambda xx : DATA_PATH + (_TREE_SAVE_FILENAME % (xx, _VERSION))
 
-TREE_AGE_MASS_AVE = 'massAveAge'
-TREE_AGE_OLDEST    = 'oldestAge'
-TREE_AGE_YOUNGEST  = 'youngestAge'
-
-TREE_AGES = [ TREE_AGE_MASS_AVE, TREE_AGE_OLDEST, TREE_AGE_YOUNGEST ]
 
 
 ###  ==============================================================  ###
@@ -102,10 +97,10 @@ def getTree(run, mergers=None, load=False, verbose=VERBOSE):
         print " - - - Finding Merger Tree from Merger Data"
 
         start = datetime.now()
-        # Find Tree
+        ### Build Tree ###
         tree = constructBHTree(run, mergers)
     
-        ### Process Tree Data ###
+        ### Analyze Tree Data ###
         timeBetween, numPast, numFuture = analyzeTree(tree, verbose=verbose)
 
         # Save Tree data
@@ -335,93 +330,6 @@ def countPastMergers( last, ind, verbose=False ):
 
 
 
-def mergerAge( mtimes, ftimes, masses, last, ind, method=TREE_AGE_MASS_AVE, verbose=False ):
-    """
-    
-    Arguments
-    ---------
-    mtimes : <double>[N], merger times
-    ftimes : <double>[N,2], first appearance times for each BH
-    masses : <double>[N,2], (corrected) merger masses of each merger BH
-    last : <long>[N,2], map to previous merger of each constituent BH
-    ind : <long>, index of target Merger
-    
-    """
-
-    assert method in TREE_AGES, "``method`` must be in '%s' !" % (str(TREE_AGES))
-    
-    last_in  = last[ind, IN_BH]
-    last_out = last[ind, OUT_BH]
-    
-    age_in  = mtimes[ind]
-    age_out = mtimes[ind]
-
-    if( verbose ): print "\n%d %e" % (ind, mtimes[ind])
-
-    
-    ### Get the effective age of constituent BHs ###
-
-    ## Get the effective Age of the 'in' BH
-
-    #   If there was a previous merger, use it's effective time
-    if( last_in  >= 0 ): age_in  -= mtimes[last_in] - mergerAge( mtimes, ftimes, masses, last, last_in, method=method, verbose=verbose )
-    #   If no previous merger, use time since seed formation
-    else:                age_in  -= ftimes[ind, IN_BH ]
-
-    if( verbose ): print "%d (%d) : IN mass = %e, lifetime = %e" % (ind, last_in, masses[ind, IN_BH], age_in)
-
-
-    ## Get the effective Age of the 'out' BH
-
-    #   If there was a previous merger, use it's effective time
-    if( last_out >= 0 ): age_out -= mtimes[last_out] - mergerAge( mtimes, ftimes, masses, last, last_out, method=method, verbose=verbose )
-    #   If no previous merger, use time since seed formation
-    else:                age_out -= ftimes[ind, OUT_BH]
-
-    if( verbose ): print "%d (%d) : OUT mass = %e, lifetime = %e" % (ind, last_out, masses[ind, OUT_BH], age_out)
-
-
-    ### Choose a Method to Measure and Age ###
-
-    # Return effective age = mass-weighted average of components
-    if(   method == TREE_AGE_MASS_AVE ):
-        useAge = (age_in*masses[ind, IN_BH] + age_out*masses[ind, OUT_BH])/np.sum(masses[ind,:])
-    # Return oldest age
-    elif( method == TREE_AGE_OLDEST ):
-        useAge = np.max([age_in, age_out])
-    # Return oldest age
-    elif( method == TREE_AGE_YOUNGEST ): 
-        useAge = np.min([age_in, age_out])
-    else:
-        raise RuntimeError("UNRECOGNIZED ``method`` '%s'!!" % (method) )
-
-
-    return useAge
-
-
-
-def getMergerAges( base, ind=214, method=TREE_AGE_MASS_AVE ):
-
-    cosmo = Cosmology()
-
-    last = base.bhtree[TREE_LAST]
-
-    ## Time of Mergers
-    mtimes = cosmo.age( base.mergers[MERGERS_TIMES] )
-
-    ## Time of BH Seed Formation
-    ftimes = base.mergerDetails[DETAILS_TIMES][:,:,DETAILS_FIRST]
-    # Fix handful of missing entries
-    for BH in [IN_BH, OUT_BH]:
-        inds = np.where( ftimes[:,BH] > 0.0 )[0]
-        if( len(inds) > 0 ): ftimes[inds,BH] = cosmo.age( ftimes[inds,BH] )[0]
-
-        inds = np.where( ftimes[:,BH] < 0.0 )[0]
-        if( len(inds) > 0 ): ftimes[inds,BH] = 0.0
-
-    print "\nResult = ", mergerAge( mtimes, ftimes, base.masses, last, ind, method=method, verbose=False)
-
-    return
 
 
 
