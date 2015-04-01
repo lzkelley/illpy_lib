4e33c7efc1bc24d20f43777575b64a8553d9594c
lzkelley <lkelley@cfa.harvard.edu>
2015-04-01 10:20:06 -0400

    Added some documentation; fixed some bugs where 'self.' was missing from member variables

--- Subhalos/StellarLifetimes.py
+++ Subhalos/StellarLifetimes.py
@@ -6,19 +6,43 @@ import scipy.interpolate
 
 class StellarLifetimes(object):
     """
+    Class to convert between stellar masses and main-sequence lifetimes, and visa-versa.
     
-    
+
     Attributes
     ----------
-    MASSES : <float>[N], array of stellar masses [grams]
-    LIFETIMES_ROT : <float>[N], array of main-sequence lifetimes for rotational (rot) stars
-    LIFETIMES_STA : <float>[N], array of main-sequence lifetimes for stationary (sta) stars
+        MASSES : <float>[N], array of stellar masses [grams]
+        LIFETIMES_ROT : <float>[N], array of main-sequence lifetimes for rotational (rot) stars
+        LIFETIMES_STA : <float>[N], array of main-sequence lifetimes for stationary (sta) stars
 
 
-    http://arxiv.org/abs/1110.5049
-    Grids of stellar models with rotation - I. Models from 0.8 to 120 Msun at solar metallicity (Z = 0.014)
-    Sylvia Ekström, Cyril Georgy, Patrick Eggenberger, et al.
-    Table 2
+    Functions
+    ---------
+        lifetime_rotational : <scalar>, 
+                              Interpolate from mass to rotational main-sequence lifetime
+        
+        lifetime_stationary : <scalar>, 
+                              Interpolate from mass to stationary (non-rotational) main-sequence lifetime
+
+        lifetime : <scalar>, 
+                   Interpolate from mass to averaged main-sequence lifetime
+
+        mass_rotational : <scalar>,
+                          Interpolate from rotational main-sequence age to stellar mass
+
+        mass_stationary : <scalar> 
+                          Interpolate from non-rotational main-sequence age to stellar mass
+
+        mass : <scalar>,
+               Interpolate from average main-sequence age to stellar mass
+
+
+    Notes
+    -----
+        http://arxiv.org/abs/1110.5049
+        Grids of stellar models with rotation - I. Models from 0.8 to 120 Msun at solar metallicity (Z = 0.014)
+        Sylvia Ekstrom, Cyril Georgy, Patrick Eggenberger, et al.
+        Table 2
 
     """
     
@@ -41,47 +65,50 @@ class StellarLifetimes(object):
 
     def __init__(self, kind='quadratic', plot=True):
         
-        self.MASSES        = np.array(_MASSES)       *_UNITS_MASS
-        self.LIFETIMES_ROT = np.array(_LIFETIMES_ROT)*_UNITS_TIME
-        self.LIFETIMES_STA = np.array(_LIFETIMES_STA)*_UNITS_TIME
+        self.MASSES        = np.array(self._MASSES)       *self._UNITS_MASS
+        self.LIFETIMES_ROT = np.array(self._LIFETIMES_ROT)*self._UNITS_TIME
+        self.LIFETIMES_STA = np.array(self._LIFETIMES_STA)*self._UNITS_TIME
 
         # Convert to Log-space
-        logMass = np.log10(MASSES)
-        logSta  = np.log10(LIFETIMES_STA)
-        logRot  = np.log10(LIFETIMES_ROT)
+        logMass = np.log10(self.MASSES)
+        logSta  = np.log10(self.LIFETIMES_STA)
+        logRot  = np.log10(self.LIFETIMES_ROT)
         logAve  = np.average([logSta, logRot], axis=0)
 
-        # Create Interpolants
-        lin_interp_time_rot = sp.interpolate.interp1d(logMass, logRot,  kind=kind)
-        lin_interp_time_sta = sp.interpolate.interp1d(logMass, logSta,  kind=kind)
-        lin_interp_time     = sp.interpolate.interp1d(logMass, logAve,  kind=kind)
-        lin_interp_mass_rot = sp.interpolate.interp1d(logRot,  logMass, kind=kind)
-        lin_interp_mass_sta = sp.interpolate.interp1d(logSta,  logMass, kind=kind)
-        lin_interp_mass     = sp.interpolate.interp1d(logAve,  logMass, kind=kind)
+        ### Create Interpolants ###
+        self._lin_interp_time_rot = sp.interpolate.interp1d(logMass, logRot, kind=kind)
+        self._lin_interp_time_sta = sp.interpolate.interp1d(logMass, logSta, kind=kind)
+        self._lin_interp_time     = sp.interpolate.interp1d(logMass, logAve, kind=kind)
+
+        # Arrays must be in order of independent variable, sort
+        inds = np.argsort(logAve)
+        self._lin_interp_mass_rot = sp.interpolate.interp1d(logRot[inds], logMass[inds], kind=kind)
+        self._lin_interp_mass_sta = sp.interpolate.interp1d(logSta[inds], logMass[inds], kind=kind)
+        self._lin_interp_mass     = sp.interpolate.interp1d(logAve[inds], logMass[inds], kind=kind)
 
 
-    def lifetime_rotational(mass): 
+    def lifetime_rotational(self, mass): 
         """Interpolate from mass to rotational main-sequence lifetime."""
-        return np.power(10.0, lin_interp_time_rot(np.log10(mass)) )
+        return np.power(10.0, self._lin_interp_time_rot(np.log10(mass)) )
         
-    def lifetime_stationary(mass): 
+    def lifetime_stationary(self, mass): 
         """Interpolate from mass to stationary (non-rotational) main-sequence lifetime."""
-        return np.power(10.0, lin_interp_time_sta(np.log10(mass)) )
+        return np.power(10.0, self._lin_interp_time_sta(np.log10(mass)) )
 
-    def lifetime(mass): 
+    def lifetime(self, mass): 
         """Interpolate from mass to averaged main-sequence lifetime."""
-        return np.power(10.0, lin_interp_time(np.log10(mass)) )
+        return np.power(10.0, self._lin_interp_time(np.log10(mass)) )
 
-    def mass_rotational(age): 
+    def mass_rotational(self, age): 
         """Interpolate from rotational main-sequence age to stellar mass"""
-        return np.power(10.0, lin_interp_mass_rot(np.log10(age)) )
+        return np.power(10.0, self._lin_interp_mass_rot(np.log10(age)) )
 
-    def mass_stationary(age): 
+    def mass_stationary(self, age): 
         """Interpolate from non-rotational main-sequence age to stellar mass"""
-        return np.power(10.0, lin_interp_mass_sta(np.log10(age)) )
+        return np.power(10.0, self._lin_interp_mass_sta(np.log10(age)) )
 
-    def mass(age): 
+    def mass(self, age): 
         """Interpolate from average main-sequence age to stellar mass"""
-        return np.power(10.0, lin_interp_mass(np.log10(age)) )
+        return np.power(10.0, self._lin_interp_mass(np.log10(age)) )
 
                                              
