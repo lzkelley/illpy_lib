0d5ec43e3d480fd5fb1785864d4591ebf3398f7d
lzkelley <lkelley@cfa.harvard.edu>
2015-04-02 15:28:39 -0400

    Added figa04

--- Subhalos/Figures/__init__.py
+++ Subhalos/Figures/__init__.py
@@ -7,6 +7,7 @@ __email__ = "lkelley@cfa.harvard.edu"
 __status__ = "Development"
 
 
-import FigA01_Subfind_SFR     as figa01
-import FigA02_Branches_SFR    as figa02
-import FigA03_EplusA_Selected as figa03
+import FigA01_Subfind_SFR          as figa01
+import FigA02_Branches_SFR         as figa02
+import FigA03_EplusA_Selected      as figa03
+import FigA04_EplusA_Evolution     as figa04
