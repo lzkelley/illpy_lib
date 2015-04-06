61dbcc3810f6b00539a41fd637b934f8d6544566
lzkelley <lkelley@cfa.harvard.edu>
2015-04-06 10:28:58 -0400

    Moved dictionary constants to Constants.py file

--- Subhalos/Analyzer.py
+++ Subhalos/Analyzer.py
@@ -29,29 +29,24 @@ NUM_RAD_BINS = 40
 RAD_EXTREMA = [ 1.0, 1.0e6 ]                                                                        # [parsec]
 
 
-NUMS_EPAS  = { 2: np.array([ 61628,  53861,  56400,  62551,   36906,  66419, 65997,  74870, 55591, 56132,
-                             32836,  47564,  15070,  71370,   83554,  36227, 24929,  59653, 74329, 55481 ]) }
+NUMS_EPAGS = { 2: np.array([ 61628,  53861,  56400,  62551, 36906,  
+                             66419,  65997,  74870,  55591, 56132, 
+                             32836,  47564,  15070,  71370, 83554,  
+                             36227,  24929,  59653,  74329, 55481 ]) }
 
 
-NUMS_NULLS = { 2: np.array([ 98645,  42640,  104561, 103399,  101025, 75064, 85549,  84796,
-                             55151,  106001, 56933,  49007,   32557,  63844, 27716,  78516,
-                             3026,   72601,  91679,  89625,   82511,  3034,  92484,  86035,
-                             104021, 66022,  78603,  104659,  78803,  98268, 115876, 97312,
-                             101598, 94810,  99410,  84564,   98779,  94954, 84278,  84121 ]) }
+NUMS_NULLS = { 2: np.array([ 98645,  42640,  104561, 103399, 101025, 
+                             75064,  85549,  84796,  55151,  106001, 
+                             56933,  49007,  32557,  63844,  27716,
+                             78516,  3026,   72601,  91679,  89625,
+                             82511,  3034,   92484,  86035,  104021, 
+                             66022,  78603,  104659, 78803,  98268, 
+                             115876, 97312,  101598, 94810,  99410,  
+                             84564,  98779,  94954,  84278,  84121 ]) }
 
 
 distConv = DIST_CONV*1000/KPC                                                                       # Convert simulation units to [pc]                  
 densConv = DENS_CONV*np.power(PC, 3.0)/MSOL                                                         # Convert simulation units to [msol/pc^3] 
-
-
-PROFILE_BIN_EDGES = 'bin_edges'
-PROFILE_BIN_AVES  = 'bin_aves'
-PROFILE_GAS       = 'gas'
-PROFILE_STARS     = 'stars'
-PROFILE_DM        = 'dm'
-PROFILE_COLS      = 'cols'
-PROFILE_CREATED   = 'created'
-PROFILE_VERSION   = 'version'
 
 
 
@@ -63,7 +58,7 @@ def main(run=RUN, snap=SNAP, loadsave=True, verbose=VERBOSE, plot=PLOT):
     # Create Radial Bins
     bins = np.logspace( *np.log10(RAD_EXTREMA), num=NUM_RAD_BINS+1 )
 
-    numsEpas  = NUMS_EPAS[run]
+    numsEpags = NUMS_EPAGS[run]
     numsNulls = NUMS_NULLS[run]
 
 
@@ -71,7 +66,7 @@ def main(run=RUN, snap=SNAP, loadsave=True, verbose=VERBOSE, plot=PLOT):
 
     if( verbose ): print " - Loading EplusA Profiles"
     start = datetime.now()
-    profsEpas = loadProfiles(run, snap, numsEpas, bins, "epas", loadsave=loadsave, verbose=verbose)
+    profsEpags = loadProfiles(run, snap, numsEpags, bins, "epags", loadsave=loadsave, verbose=verbose)
     stop = datetime.now()
     if( verbose ): print " - - Loaded after %s" % (str(stop-start))
 
@@ -85,11 +80,14 @@ def main(run=RUN, snap=SNAP, loadsave=True, verbose=VERBOSE, plot=PLOT):
     if( verbose ): print " - - Loaded after %s" % (str(stop-start))
 
 
+    ### Plot ###
+    Figures.figa06.plotFigA06_EplusA_Profiles(run, profsEpags, profsNulls)
+
 
     stopMain = datetime.now()
     if( verbose ): print " - Done after %s" % (str(stopMain-startMain))
     
-    return
+    return profsEpags, profsNulls
 
 # main()
 
