0793e81fbd60a8176f795e23caf7640a5993da21
lzkelley <lkelley@cfa.harvard.edu>
2015-04-10 11:30:19 -0400

    Fixed setup to correctly build inplace files.

--- illpy/illbh/setup.py
+++ illpy/illbh/setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from distutils.extension import Extension
+
+ext_modules=[
+    Extension("MatchDetails",  ["MatchDetails.pyx"] ),
+    Extension("BuildTree",     ["BuildTree.pyx"]    ),
+]
 
 setup(
-    ext_modules = cythonize("*.pyx"),
-    )
+  name = 'MyProject',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules,
+)
+
