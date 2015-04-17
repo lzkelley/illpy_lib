696e2f94a556745683695df7c556441fea0a89ca
lzkelley <lkelley@cfa.harvard.edu>
2015-04-17 00:12:40 -0400

    Place constants in more logical places

--- Subhalos/Constants.py
+++ Subhalos/Constants.py
@@ -5,12 +5,61 @@ Provide common constants and hard-settings.
 
 import numpy as np
 
-
+import illpy
+from illpy.Constants import *
+from illpy.illbh.BHConstants import *
 
 BASE_PATH  = "/n/home00/lkelley/illustris/EplusA/Subhalos/"
 
 TYPE_ID     = np.uint64
 TYPE_SCALAR = np.float64
+
+
+
+
+### Subhalo Properties ###
+SUBHALO_ID                        = "id"
+SUBHALO_RUN                       = "run"
+SUBHALO_SNAPSHOT                  = "snapshot"
+SUBHALO_CREATED                   = "created"
+SUBHALO_VERSION                   = "version"
+
+SNAPSHOT_BH_MASS                  = "BH_Mass"
+SNAPSHOT_BH_HSML                  = "BH_Hsml"
+SNAPSHOT_BH_MDOT                  = "BH_Mdot"
+SNAPSHOT_STELLAR_PHOTOS           = "GFM_StellarPhotometrics"
+SNAPSHOT_FORM_TIME                = "GFM_StellarFormationTime"
+SNAPSHOT_PARENT                   = "ParentID"
+SNAPSHOT_SUBFIND_HSML             = "SubfindHsml"
+SNAPSHOT_SUBFIND_VDISP            = "SubfindVelDisp"
+SNAPSHOT_FILENAME                 = "filename"
+SNAPSHOT_HSML                     = "hsml"
+SNAPSHOT_MASS                     = "mass"
+SNAPSHOT_MASSES                   = "masses"
+SNAPSHOT_NPART                    = "npart_loaded"
+SNAPSHOT_POS                      = "pos"
+SNAPSHOT_POT                      = "pot"
+SNAPSHOT_DENS                     = "rho"
+SNAPSHOT_SFR                      = "sfr"
+SNAPSHOT_VEL                      = "vel"
+SNAPSHOT_EINT                     = "internalenergy"
+
+
+SNAPSHOT_PROPERTIES = [ SNAPSHOT_BH_MASS, SNAPSHOT_BH_HSML, SNAPSHOT_BH_MDOT, SNAPSHOT_STELLAR_PHOTOS, 
+                        SNAPSHOT_FORM_TIME, SNAPSHOT_PARENT, SNAPSHOT_SUBFIND_HSML, SNAPSHOT_SUBFIND_VDISP,
+                        SNAPSHOT_FILENAME, SNAPSHOT_HSML, SNAPSHOT_MASS, SNAPSHOT_MASSES, 
+                        SNAPSHOT_NPART, SNAPSHOT_POS, SNAPSHOT_POT, SNAPSHOT_DENS, 
+                        SNAPSHOT_SFR, SNAPSHOT_VEL, SNAPSHOT_EINT ]
+
+_SUBHALO_PARTICLES_PATH_BASE     = "subhalos/snap_%d/"
+_SUBHALO_PARTICLES_FILENAME_BASE = "ill-%d_snap-%d_subhalo-%d.npz"
+
+
+def GET_SUBHALO_PARTICLES_FILENAMES(run, snap, subhalo): 
+    fileName  = GET_PROCESSED_DIR(run) + _SUBHALO_PARTICLES_PATH_BASE % (snap)
+    fileName += _SUBHALO_PARTICLES_FILENAME_BASE % (run, snap, subhalo)
+    return fileName
+
 
 
 ### General Illustris ###
@@ -21,20 +70,6 @@ ILLUSTRIS_RUN_NAMES           = { 1: 'L75n1820FP',
                                   2: 'L75n910FP' ,
                                   3: 'L75n455FP'  }
 
-ILLUSTRIS_OUTPUT_PATH_BASE    = '/n/hernquistfs1/Illustris/Runs/%s/output/'
-ILLUSTRIS_OUTPUT_PATHS        = lambda run: ILLUSTRIS_OUTPUT_PATH_BASE % (ILLUSTRIS_RUN_NAMES[run])
-
-ILLUSTRIS_OUTPUT_GROUP_PATH_BASE   = '/n/ghernquist/Illustris/Runs/%s/output/groups_%d/'
-ILLUSTRIS_OUTPUT_GROUP_PATHS       = lambda run,snap: ILLUSTRIS_OUTPUT_GROUP_PATH_BASE % (ILLUSTRIS_RUN_NAMES[run],snap)
-ILLUSTRIS_OUTPUT_GROUP_FILENAME_BASE   = 'fof_subhalo_tab_%d.0.hdf5'
-ILLUSTRIS_OUTPUT_GROUP_FILENAMES       = lambda snap: ILLUSTRIS_OUTPUT_GROUP_FILENAME_BASE % (snap)
-ILLUSTRIS_OUTPUT_GROUP_FIRST_FILENAME  = lambda run,snap: ILLUSTRIS_OUTPUT_GROUP_PATHS(run,snap) + ILLUSTRIS_OUTPUT_GROUP_FILENAMES(snap)
-
-ILLUSTRIS_OUTPUT_SNAPSHOT_PATH_BASE = '/n/ghernquist/Illustris/Runs/%s/output/snapdir_%d/'
-ILLUSTRIS_OUTPUT_SNAPSHOT_PATHS       = lambda run,snap: ILLUSTRIS_OUTPUT_SNAPSHOT_PATH_BASE % (ILLUSTRIS_RUN_NAMES[run],snap)
-ILLUSTRIS_OUTPUT_SNAPSHOT_FILENAME_BASE   = 'snap_%d.0.hdf5'
-ILLUSTRIS_OUTPUT_SNAPSHOT_FILENAMES       = lambda snap: ILLUSTRIS_OUTPUT_SNAPSHOT_FILENAME_BASE % (snap)
-ILLUSTRIS_OUTPUT_SNAPSHOT_FIRST_FILENAME  = lambda run,snap: ILLUSTRIS_OUTPUT_SNAPSHOT_PATHS(run,snap) + ILLUSTRIS_OUTPUT_SNAPSHOT_FILENAMES(snap)
 
 
 
@@ -82,37 +117,6 @@ BRANCH_SNAPS                  = "snapshots"
 
 
 ### EplusA Snapshot Particle Data ###
-SUBHALO_ID                        = "id"
-SUBHALO_RUN                       = "run"
-SUBHALO_SNAPSHOT                  = "snapshot"
-SUBHALO_CREATED                   = "created"
-
-SNAPSHOT_BH_MASS                  = "BH_Mass"
-SNAPSHOT_BH_HSML                  = "BH_Hsml"
-SNAPSHOT_BH_MDOT                  = "BH_Mdot"
-SNAPSHOT_STELLAR_PHOTOS           = "GFM_StellarPhotometrics"
-SNAPSHOT_FORM_TIME                = "GFM_StellarFormationTime"
-SNAPSHOT_PARENT                   = "ParentID"
-SNAPSHOT_SUBFIND_HSML             = "SubfindHsml"
-SNAPSHOT_SUBFIND_VDISP            = "SubfindVelDisp"
-SNAPSHOT_FILENAME                 = "filename"
-SNAPSHOT_HSML                     = "hsml"
-SNAPSHOT_MASS                     = "mass"
-SNAPSHOT_MASSES                   = "masses"
-SNAPSHOT_NPART                    = "npart_loaded"
-SNAPSHOT_POS                      = "pos"
-SNAPSHOT_POT                      = "pot"
-SNAPSHOT_DENS                     = "rho"
-SNAPSHOT_SFR                      = "sfr"
-SNAPSHOT_VEL                      = "vel"
-SNAPSHOT_EINT                     = "internalenergy"
-
-
-SNAPSHOT_PROPERTIES = [ SNAPSHOT_BH_MASS, SNAPSHOT_BH_HSML, SNAPSHOT_BH_MDOT, SNAPSHOT_STELLAR_PHOTOS, 
-                        SNAPSHOT_FORM_TIME, SNAPSHOT_PARENT, SNAPSHOT_SUBFIND_HSML, SNAPSHOT_SUBFIND_VDISP,
-                        SNAPSHOT_FILENAME, SNAPSHOT_HSML, SNAPSHOT_MASS, SNAPSHOT_MASSES, 
-                        SNAPSHOT_NPART, SNAPSHOT_POS, SNAPSHOT_POT, SNAPSHOT_DENS, 
-                        SNAPSHOT_SFR, SNAPSHOT_VEL, SNAPSHOT_EINT ]
 
 
 
@@ -131,20 +135,12 @@ PROFILE_VERSION   = 'version'
 
 ### Intermediate Save Files ###
 
-_SUBHALO_PARTICLES_PATH_BASE     = BASE_PATH + "data/%s/subhalos/snap_%d/"
-_SUBHALO_PARTICLES_FILENAME_BASE = "ill-%d_snap-%d_subhalo-%d.npz"
-
 _SUBHALO_BRANCHES_SAVE_BASE = BASE_PATH + "data/%s/ill-%d_branches.npz"
 
 _SUBHALO_FILENAMES_NUMBERS_SAVE_BASE = BASE_PATH + "data/%s/ill-%d_snap-%d_subhalos-names-numbers.npz"
 
-_SUBHALO_RADIAL_PROFILES_SAVE_BASE = BASE_PATH + "data/%s/ill-%d_snap-%d_subhalos-profiles_%s.npz"
+_SUBHALO_RADIAL_PROFILES_FILENAME_BASE = BASE_PATH + "data/%s/ill-%d_snap-%d_subhalos-profiles.npz"
 
-
-def SUBHALO_PARTICLES_FILENAMES(run, snap, subhalo): 
-    fileName  = _SUBHALO_PARTICLES_PATH_BASE % (ILLUSTRIS_RUN_NAMES[run], snap)
-    fileName += _SUBHALO_PARTICLES_FILENAME_BASE % (run, snap, subhalo)
-    return fileName
 
 
 def SUBHALO_BRANCHES_FILENAMES(run):
@@ -157,6 +153,6 @@ def SUBHALO_FILENAMES_NUMBERS_FILENAMES(run, snap):
     return fileName
 
 
-def SUBHALO_RADIAL_PROFILES_FILENAMES(run, snap, name):
-    fileName = _SUBHALO_RADIAL_PROFILES_SAVE_BASE % (ILLUSTRIS_RUN_NAMES[run], run, snap, name)
+def GET_SUBHALO_RADIAL_PROFILES_FILENAMES(run, snap):
+    fileName = _SUBHALO_RADIAL_PROFILES_FILENAME_BASE % (ILLUSTRIS_RUN_NAMES[run], run, snap)
     return fileName
