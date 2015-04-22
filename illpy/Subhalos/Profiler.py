67ebe3d72f6b7c28abaf912a6a7a28b71c444255
lzkelley <lkelley@cfa.harvard.edu>
2015-04-22 14:14:57 -0400

    Significant improvements

--- illpy/Subhalos/Profiler.py
+++ illpy/Subhalos/Profiler.py
@@ -8,6 +8,9 @@ import sys
 import os
 from datetime import datetime
 
+import numpy as np
+import scipy as sp
+
 import illpy
 from illpy import Cosmology
 from illpy.Constants import *
@@ -22,12 +25,10 @@ _VERSION = 0.1
 NUM_RAD_BINS = 40
 RAD_EXTREMA = [ 1.0, 1.0e6 ]                      # [parsecs]
 
-distConv = DIST_CONV
-densConv = DENS_CONV
 
+def getSubhaloRadialProfiles(data, bins=None, nbins=NUM_RAD_BINS, verbose=VERBOSE):
 
-
-def getSubhaloRadialProfiles(data, bins=None, verbose=VERBOSE):
+    if( verbose ): print " - - Profiler.getSubhaloRadialProfiles()"
 
     numParts = data[SNAPSHOT_NPART]
     cumNumParts = np.concatenate([[0],np.cumsum(numParts)])
@@ -36,43 +37,65 @@ def getSubhaloRadialProfiles(data, bins=None, verbose=VERBOSE):
     # Get Number of Particles
     ngas      = data[SNAPSHOT_NPART][PARTICLE_TYPE_GAS]
     nstars    = data[SNAPSHOT_NPART][PARTICLE_TYPE_STAR]
+    ndm       = data[SNAPSHOT_NPART][PARTICLE_TYPE_DM]
+
+    if( verbose ): print " - - - Number of DM %d, Stars %d, Gas %d" % (ndm, nstars, ngas)
+
+
+    ### Load Masses, Densities and Create Profiles ###
+    rhoGas    = data[SNAPSHOT_DENS]*DENS_CONV      # Only gas has 'density'/'rho'
+    massGas   = data[SNAPSHOT_MASS][:ngas]*MASS_CONV
+    massStars = data[SNAPSHOT_MASS][ngas:(ngas+nstars)]*MASS_CONV
+    massDM    = data[SNAPSHOT_MASSES][PARTICLE_TYPE_DM]*MASS_CONV
+
+
+    posAll = data[SNAPSHOT_POS]
+    posAll = reflectPos(posAll)
+
 
     ### Get Positions and Radii of Each Particle ###
     bhmass = data[SNAPSHOT_BH_MASS]
-    posBH = data[SNAPSHOT_POS][slices[PARTICLE_TYPE_BH  ]]
+    posBH = posAll[slices[PARTICLE_TYPE_BH  ]]
     # If there are multiple BHs, use the most-massive as the CoM
     if( len(bhmass) > 1 ):
         bhind = np.argmax(bhmass)
         posBH = posBH[bhind]
 
-    # Subtract off BH position (i.e. define BH as center)
-    posGas    = data[SNAPSHOT_POS][slices[PARTICLE_TYPE_GAS ]] - posBH
-    posDM     = data[SNAPSHOT_POS][slices[PARTICLE_TYPE_DM  ]] - posBH
-    posStars  = data[SNAPSHOT_POS][slices[PARTICLE_TYPE_STAR]] - posBH
 
-    radsGas   = np.array([ aux.getMagnitude(vec) for vec in posGas   ])*distConv
-    radsDM    = np.array([ aux.getMagnitude(vec) for vec in posDM    ])*distConv
-    radsStars = np.array([ aux.getMagnitude(vec) for vec in posStars ])*distConv
+    posGas    = posAll[slices[PARTICLE_TYPE_GAS ]]
+    posDM     = posAll[slices[PARTICLE_TYPE_DM  ]]
+    posStars  = posAll[slices[PARTICLE_TYPE_STAR]]
+
+
+
+    # Select a center position
+    # posCenter = posBH
+    # posCenter = posAve
+    posCenter = np.median(posStars, axis=0)
+
+    # Subtract off BH position
+    posGas   -= posCenter
+    posDM    -= posCenter
+    posStars -= posCenter
+
+    radsGas   = np.array([ aux.getMagnitude(vec) for vec in posGas   ])*DIST_CONV
+    radsDM    = np.array([ aux.getMagnitude(vec) for vec in posDM    ])*DIST_CONV
+    radsStars = np.array([ aux.getMagnitude(vec) for vec in posStars ])*DIST_CONV
 
     # Create Radial Bins if they are not provided
     if( bins is None ):
         radsExtr  = np.concatenate([radsGas, radsDM, radsStars])
         radsExtr  = aux.extrema(radsExtr, nonzero=True)
-        bins  = np.logspace( *np.log10(radsExtr), num=NUM_RAD_BINS+1 )
+        bins      = np.logspace( *np.log10(radsExtr), num=nbins+1 )
 
 
     # Find average bin positions, and radial bin (shell) volumes
-    binAves = np.zeros(NUM_RAD_BINS)
-    binVols = np.zeros(NUM_RAD_BINS)
+    binAves = np.zeros(nbins)
+    binVols = np.zeros(nbins)
     for ii in range(len(bins)-1):
         binAves[ii] = np.average([bins[ii], bins[ii+1]])
         binVols[ii] = np.power(bins[ii+1],3.0) - np.power(bins[ii],3.0)
 
-
-    ### Load Masses, Densities and Create Profiles ###
-    rhoGas    = data[SNAPSHOT_DENS]*densConv      # Only gas has 'density'/'rho'
-    massStars = data[SNAPSHOT_MASS][ngas:(ngas+nstars)]*MASS_CONV
-    massDM    = data[SNAPSHOT_MASSES][PARTICLE_TYPE_DM]*MASS_CONV
 
     colsStars = data[SNAPSHOT_STELLAR_PHOTOS]     # Only stars have photometric entries
     gmr       = np.array([ cols[PHOTO_g] - cols[PHOTO_r] for cols in colsStars])
@@ -96,3 +119,188 @@ def getSubhaloRadialProfiles(data, bins=None, verbose=VERBOSE):
     return bins, binAves, hg, hs, hd, hc
 
 # getSubhaloRadialProfiles()
+
+
+
+
+def reflectPos(pos, center=None):
+    """
+    Given a set of position vectors, reflect those which are on the wrong edge of the box.
+
+    NOTE: Input positions ``pos`` MUST BE GIVEN IN illustris simulation units: [ckpc/h] !!!!
+    If a particular ``center`` point is not given, the median position is used.
+    
+    Arguments
+    ---------
+    pos    : <float>[N,3], array of ``N`` vectors, MUST BE IN SIMULATION UNITS
+    center : <float>[3],   (optional=None), center coordinates, defaults to median of ``pos``
+
+    Returns
+    -------
+    fix    : <float>[N,3], array of 'fixed' positions with bad elements reflected
+
+    """
+
+    FULL = BOX_LENGTH
+    HALF = 0.5*FULL
+
+    # Create a copy of input positions
+    fix = np.array(pos)
+
+    # Use median position as center if not provided
+    if( center is None ): center = np.median(fix, axis=0)
+    else:                 center = ref
+
+    # Find distances to center
+    offsets = fix - center
+
+    # Reflect positions which are more than half a box away
+    fix[offsets >  HALF] -= FULL
+    fix[offsets < -HALF] += FULL
+
+    return fix
+
+
+
+
+
+
+def powerLaw_ll(lr,ly0,lr0,alpha):
+    """ log-log transform of ``n(r) = y0*(r/r0)^alpha`` """
+    return alpha*(lr + ly0 - lr0)
+
+def powerLaw(rr,y0,r0,alpha):
+    """ Single power law ``n(r) = y0*(r/r0)^alpha`` """
+    return y0*np.power(rr/r0, alpha)
+
+def brokenPowerLaw_ll(rr,y0,r0,alpha,beta):
+    """ Log-log transform of a broken (piece-wise defined) power-law """
+    y1 = (powerLaw_ll(rr,y0,r0,alpha))[rr<=r0]
+    y2 = (powerLaw_ll(rr,y0,r0,beta ))[rr> r0]
+    yy = np.concatenate((y1,y2))
+    return yy
+
+def brokenPowerLaw(rr,y0,r0,alpha,beta):
+    """ Two power-laws linked together piece-wise at the scale radius ``r0`` """
+    y1 = (powerLaw(rr,y0,r0,alpha))[rr<=r0]
+    y2 = (powerLaw(rr,y0,r0,beta ))[rr> r0]
+    yy = np.concatenate((y1,y2))
+    return yy
+
+
+
+
+
+def fit_powerLaw(xx, yy, pars=None):
+    """
+    Fit the given data with a single power-law function
+    
+    Notes: the data is first transformed into log-log space, where a linear
+           function is fit.  That is transformed back into linear-space and
+           returned.
+           
+    Arguments
+    ---------
+    xx : <float>[N], independent variable given in normal (linear) space
+    yy : <float>[N],   dependent variable given in normal (linear) space
+    
+    Returns
+    -------
+    func  : <callable>, fitting function with the fit parameters already plugged-in
+    pars1 : <float>[2], fit parameters defining the power-law function.
+    """
+    
+    # Transform to log-log space and scale towards unity
+    scx = np.max(xx)
+    scy = np.max(yy)
+    
+    lx = np.log10(xx/scx)
+    ly = np.log10(yy/scy)
+    
+    # Guess Power Law Parameters
+    if( pars is None ): 
+        pars0 = [1.0, -3.0]
+    else:                
+        pars0 = np.array(pars)
+        pars0[0] = np.log10(pars0[0]/scx)
+
+
+    pars1, covar = sp.optimize.curve_fit(powerLaw_ll, lx, ly, p0=pars0)
+    
+    # Transform fit parameters from log-log space, back to normal
+    pars1[0] = scx*np.power(10.0, pars1[0])
+
+    # Create fitting function
+    func = lambda rr: scy*powerLaw(rr, *pars1)
+    
+    # Return function and fitting parameters
+    return func, pars1
+
+
+
+def fit_powerLaw_broken(xx, yy, pars=None):
+    # Transform to log-log space and scale towards unity
+    scx = 1.0 #np.max(xx)
+    scy = 1.0 #np.max(yy)
+    
+    lx = np.log10(xx/scx)
+    ly = np.log10(yy/scy)
+    
+    
+    # Guess Power Law Parameters
+    if( pars is None ): 
+        pars0 = [1.0, -1.0, -4.0]
+    else:
+        pars0 = np.array(pars)
+        pars0[0] = np.log10(pars0[0]/scx)
+    
+
+    pars1, covar = sp.optimize.curve_fit(brokenPowerLaw_ll, lx, ly, p0=pars0)
+    
+    # Transform fit parameters from log-log space, back to normal
+    pars1[0] = scx*np.power(10.0, pars1[0])
+
+    # Create fitting function
+    func = lambda rr: scy*brokenPowerLaw(rr, *pars1)
+    
+    # Return function and fitting parameters
+    return func, pars1
+
+
+
+def fit_powerLaw_innerFixed(xx, yy, fix, pars=None):
+    # Transform to log-log space and scale towards unity
+    scx = np.max(xx)
+    scy = 1.0 #np.max(yy)
+    
+    lx = np.log10(xx/scx)
+    ly = np.log10(yy/scy)
+    
+    
+    # Guess Power Law Parameters
+    if( pars is None ): 
+        pars0 = [1.0, -1.0, -4.0]
+    else:
+        pars0 = np.array(pars)
+        pars0[0] = np.log10(pars0[0]/scx)
+
+
+    inFix = lambda rr,r0,beta: brokenPowerLaw_ll(rr, r0, fix, beta)
+    pars1, covar = sp.optimize.curve_fit(inFix, lx, ly, p0=[pars0[0],pars0[2]])
+    
+    # Transform fit parameters from log-log space, back to normal
+    pars1[0] = scx*np.power(10.0, pars1[0])
+
+    # Create fitting function
+    func = lambda rr: scy*brokenPowerLaw(rr, pars1[0], fix, pars1[1])
+
+
+    #pars1[0] /= np.power(scy, 1.0/pars1[1])
+    #func = lambda rr: brokenPowerLaw(rr, pars1[0], fix, pars1[1])
+
+    
+    # Return function and fitting parameters
+    return func, np.insert(pars1, 1, fix)
+
+
+    
