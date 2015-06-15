4362efa9d4e6ae38c4a6010c07b9e7d2b282fc34
lzkelley <lkelley@cfa.harvard.edu>
2015-06-15 13:06:52 -0400

    Moved particle parameters (key strings) into 'PARTICLE' class, and created class 'SUBHALO' for group catalog parameters

--- illpy/Subhalos/Constants.py
+++ illpy/Subhalos/Constants.py
@@ -1,94 +1,81 @@
-"""
-Provide common constants and hard-settings.
-
-"""
-
-import numpy as np
-
-import illpy
-from illpy.illbh.BHConstants import GET_PROCESSED_DIR
-
-TYPE_ID     = np.uint64
-TYPE_SCALAR = np.float64
 
 
-### Subhalo Properties ###
-SUBHALO_ID                        = "id"
-SUBHALO_RUN                       = "run"
-SUBHALO_SNAPSHOT                  = "snapshot"
-SUBHALO_CREATED                   = "created"
-SUBHALO_VERSION                   = "version"
+class SNAPSHOT():
+    IDS                  = "ParticleIDs"
+    POS                  = "Coordinates"
 
-SNAPSHOT_BH_MASS                  = "BH_Mass"
-SNAPSHOT_BH_HSML                  = "BH_Hsml"
-SNAPSHOT_BH_MDOT                  = "BH_Mdot"
-SNAPSHOT_STELLAR_PHOTOS           = "GFM_StellarPhotometrics"
-SNAPSHOT_FORM_TIME                = "GFM_StellarFormationTime"
-SNAPSHOT_PARENT                   = "ParentID"
-SNAPSHOT_SUBFIND_HSML             = "SubfindHsml"
-SNAPSHOT_SUBFIND_VDISP            = "SubfindVelDisp"
-SNAPSHOT_FILENAME                 = "filename"
-SNAPSHOT_HSML                     = "hsml"
-SNAPSHOT_MASS                     = "mass"
-SNAPSHOT_MASSES                   = "masses"
-SNAPSHOT_NPART                    = "npart_loaded"
-SNAPSHOT_POS                      = "pos"
-SNAPSHOT_POT                      = "pot"
-SNAPSHOT_DENS                     = "rho"
-SNAPSHOT_SFR                      = "sfr"
-SNAPSHOT_VEL                      = "vel"
-SNAPSHOT_EINT                     = "internalenergy"
+    POT                  = "Potential"
+    DENS                 = "Denity"
+    SFR                  = "StarFormationRate"
+    VEL                  = "Velocities"
+    EINT                 = "InternalEnergy"
+    MASS                 = "Masses"
+
+    HSML                 = "SmoothingLength"      # 2x max triangle radius
+
+    SUBF_HSML            = "SubfindHsml"
+    SUBF_VDISP           = "SubfindVelDisp"
 
 
-SNAPSHOT_PROPERTIES = [ SNAPSHOT_BH_MASS, SNAPSHOT_BH_HSML, SNAPSHOT_BH_MDOT, SNAPSHOT_STELLAR_PHOTOS, 
-                        SNAPSHOT_FORM_TIME, SNAPSHOT_PARENT, SNAPSHOT_SUBFIND_HSML, SNAPSHOT_SUBFIND_VDISP,
-                        SNAPSHOT_FILENAME, SNAPSHOT_HSML, SNAPSHOT_MASS, SNAPSHOT_MASSES, 
-                        SNAPSHOT_NPART, SNAPSHOT_POS, SNAPSHOT_POT, SNAPSHOT_DENS, 
-                        SNAPSHOT_SFR, SNAPSHOT_VEL, SNAPSHOT_EINT ]
+    '''
+    BH_MASS                  = "BH_Mass"
+    BH_HSML                  = "BH_Hsml"
+    BH_MDOT                  = "BH_Mdot"
+    STELLAR_PHOTOS           = "GFM_StellarPhotometrics"
+    FORM_TIME                = "GFM_StellarFormationTime"
 
-_SUBHALO_PARTICLES_PATH_BASE     = "subhalos/snap_%d/"
-_SUBHALO_PARTICLES_FILENAME_BASE = "ill-%d_snap-%d_subhalo-%d.npz"
-
-
-def GET_SUBHALO_PARTICLES_FILENAMES(run, snap, subhalo): 
-    fileName  = GET_PROCESSED_DIR(run) + _SUBHALO_PARTICLES_PATH_BASE % (snap)
-    fileName += _SUBHALO_PARTICLES_FILENAME_BASE % (run, snap, subhalo)
-    return fileName
+    PARENT                   = "ParentID"
+    NPART                    = "npart_loaded"
+    '''
 
 
-### Subfind Catalog Parameters ###
 
-SH_BH_MASS                    = "SubhaloBHMass"
-SH_RAD_TYPE                   = "SubhaloHalfmassRadType"
-SH_MASS_TYPE                  = "SubhaloMassType"
-SH_SFR                        = "SubhaloSFR"
-SH_PHOTO                      = "SubhaloStellarPhotometrics"
-SH_VEL_DISP                   = "SubhaloVelDisp"
-SH_SPIN                       = "SubhaloSpin"
-SH_LEN_TYPE                   = "SubhaloLenType"
-SH_GROUP_NUM                  = "SubhaloGrNr"
-SH_FILENAME                   = "filebase"
-SH_PARENT                     = "SubhaloParent"
+class SUBHALO():
 
+    POS                  = "SubhaloPos"
+    # SubhaloBHMdot
+    VMAX                 = "SubhaloVmax"
+    # SubhaloWindMass
+    # SubhaloGasMetallicityMaxRad
+    VDISP                = "SubhaloVelDisp"
+    SFR                  = "SubhaloSFR"
+    # SubhaloStarMetallicityMaxRad
+    NUM_PARTS            = "SubhaloLen"
+    SFR_HALF_RAD         = "SubhaloSFRinHalfRad"
+    PHOTOS               = "SubhaloStellarPhotometrics"
+    METZ                 = "SubhaloGasMetallicity"
+    BH_MASS              = "SubhaloBHMass"
+    MOST_BOUND           = "SubhaloIDMostbound"
+    MASS_TYPE            = "SubhaloMassType"
+    # SubhaloStellarPhotometricsMassInRad
+    RAD_HALF_MASS        = "SubhaloHalfmassRad"
+    # SubhaloParent
+    # SubhaloSpin
+    # SubhaloStarMetallicityHalfRad
+    VEL                  = "SubhaloVel"
+    NUM_PARTS_TYPE       = "SubhaloLenType"
+    # SubhaloGasMetallicitySfrWeighted
+    # SubhaloGasMetallicityHalfRad
+    # SubhaloMassInRad
+    NUM_GROUP            = "SubhaloGrNr"
+    # SubhaloMassInHalfRad
+    # SubhaloSFRinRad
+    # SubhaloMassInMaxRad
+    # SubhaloHalfmassRadType
+    # SubhaloMassInMaxRadType
+    COM                  = "SubhaloCM"
+    # SubhaloStarMetallicity
+    # SubhaloMassInHalfRadType
+    MASS                 = "SubhaloMass"
+    # SubhaloMassInRadType
+    RAD_VMAX             = "SubhaloVmaxRad"
+    # SubhaloSFRinMaxRad
+    # SubhaloStellarPhotometricsRad
+    # SubhaloGasMetallicitySfr
 
-SUBFIND_PARAMETERS            = [ SH_BH_MASS,  SH_RAD_TYPE, SH_MASS_TYPE, SH_SFR,       SH_PHOTO, 
-                                  SH_VEL_DISP, SH_LEN_TYPE, SH_SPIN,      SH_GROUP_NUM, SH_PARENT ]
+    @staticmethod
+    def PROPERTIES(): 
+        return [getattr(SUBHALO,it) for it in vars(SUBHALO) 
+                if not it.startswith('_') and not callable(getattr(SUBHALO,it)) ]
 
-SUBFIND_PARAMETER_TYPES       = [ TYPE_SCALAR, TYPE_SCALAR, TYPE_SCALAR,  TYPE_SCALAR,  TYPE_SCALAR, 
-                                  TYPE_SCALAR, TYPE_ID,     TYPE_SCALAR,  TYPE_ID,      TYPE_ID    ]
-
-
-SH_SNAPSHOT_NUM               = "snapshot"
-
-
-### Subhalo Profiles ###
-
-PROFILE_BIN_EDGES = 'bin_edges'
-PROFILE_BIN_AVES  = 'bin_aves'
-PROFILE_GAS       = 'gas'
-PROFILE_STARS     = 'stars'
-PROFILE_DM        = 'dm'
-PROFILE_COLS      = 'cols'
-PROFILE_CREATED   = 'created'
-PROFILE_VERSION   = 'version'
-
+# } class SUBHALO
