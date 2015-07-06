b4e1a821ea673e1ab5e452f7fd13fe7252450f43
lzkelley <lkelley@cfa.harvard.edu>
2015-07-06 11:53:14 -0400

    Significant streamlining.  When the offset table is first created, BHs are associated with their host Halos and Subhalos --- this data is stored (and saved) in the talbe, removing the need for functions to do this on demand.

--- illpy/Subhalos/HaloOffsetTable.py
+++ illpy/Subhalos/HaloOffsetTable.py
@@ -1,66 +1,64 @@
 """
 Manage table of particle offsets for associating particles with halos and subhalos.
 
-The table is in the form of a dictionary with keys given by the constants ``OFFSET_``.  The method
-``loadOffsetTable()`` is the only necessary API - it deals with constructing, saving, and loading
-the offset tables.
+The table is in the form of a dictionary with keys given by the values of the ``OFFTAB`` class.
+The method ``loadOffsetTable()`` is the only necessary API - it deals with constructing, saving, 
+and loading the offset table.
 
 Classes
 -------
-   OFFSET : enumerator-like class for dictionary key-words
+    OFFTAB : enumerator-like class for dictionary key-words
 
 
 Functions
 ---------
-   loadOffsetTable                 : load offset table for target run and snapshot
-   getBHSubhaloIndices             : get the subhalo indices for given blackhole IDs
+    loadOffsetTable            : load offset table for target run and snapshot
 
-   _GET_OFFSET_TABLE_SAVE_FILENAME : filename which the offset table is saved/loaded to/from
-   _getHostsFromParticleIndex      : Given a BH index, find its host halo and subhalo
-   _bhIDsToIndices                 : convert from BH ID number to Index in snapshot files
-   _constructOffsetTable           : construct the offset table from the group catalog
-   _constructBHIndexTable          : construct mapping from BH IDs to indices in snapshot files
+    _GET_OFFSET_TABLE_FILENAME : filename which the offset table is saved/loaded to/from
+    _constructOffsetTable      : construct the offset table from the group catalog
+    _constructBHIndexTable     : construct mapping from BH IDs to indices in snapshot files
 
 
 Notes
 -----
-The structure of the table is 3 different arrays with corresponding entries.
-``halos``     (``OFFSET_HALOS``)     : <int64>[N],   halo number
-``subhalos``  (``OFFSET_SUBHALOS``)  : <int64>[N],   subhalo number
-``particles`` (``OFFSET_PARTICLES``) : <int64>[N,6], particle offsets for each halo/subhalo
+    The structure of the table is 3 different arrays with corresponding entries.
+    ``halos``     (``OFFTAB.HALOS``)    : <int>[N],   halo number
+    ``subhalos``  (``OFFTAB.SUBHALOS``) : <int>[N],   subhalo number
+    ``particles`` (``OFFTAB.OFFSETS``)  : <int>[N,6], particle offsets for each halo/subhalo
 
-The table is ordered in the same way as the snapshots, where particles are grouped into subhalos,
-which belong to halos.  Each halo also has (or can have) a group of particles not in any subhalo.
-Finally, the last entry is for particles with no halo and no subhalo.  When there is no match for
-a subhalo or halo, the corresponding number is listed as '-1'.
+    The table is ordered in the same way as the snapshots, where particles are grouped into subhalos,
+    which belong to halos.  Each halo also has (or can have) a group of particles not in any subhalo.
+    Finally, the last entry is for particles with no halo and no subhalo.  When there is no match for
+    a subhalo or halo, the corresponding number is listed as '-1'.
 
-For a halo 'i', with NS_i subhalos, there are NS_i+1 entries for that halo.
-If the total number of subhalos is NS = SUM_i( NS_i ), and there are
-NH halos, then the total number of entries is NS + NH + 1.
+    For a halo 'i', with NS_i subhalos, there are NS_i+1 entries for that halo.
+    If the total number of subhalos is NS = SUM_i( NS_i ), and there are
+    NH halos, then the total number of entries is NS + NH + 1.
 
-This is what the table looks like (using made-up numbers):
+    This is what the table looks like (using made-up numbers):
 
-                        PARTICLES {0,5}
-    HALO    SUBHALO     0     1  ...   5
-  | ====================================
-  |    0          0     0     0  ...   0  <-- halo-0, subhalo-0, no previous particles
-  |    0          1    10     4  ...   1  <--  first part0 for this subhalo is 10th part0 overall
-  |    0          2    18     7  ...   3  <--  first part1 for this subhalo is  7th part1 overall
-  |                              ...
-  |    0         -1   130    58  ...  33  <-- particles of halo-0, no subhalo
-  |
-  |    1         22   137    60  ...  35  <-- In halo-0 there were 22 subhalos and 137 part0, etc
-  |    1         23              ...
-  |                              ...
-  |                              ...
-  |   -1         -1  2020   988  ... 400
-  | ====================================
+                            PARTICLES {0,5}
+        HALO    SUBHALO     0     1  ...   5
+      | ====================================
+      |    0          0     0     0  ...   0  <-- halo-0, subhalo-0, no previous particles
+      |    0          1    10     4  ...   1  <--  first part0 for this subhalo is 10th part0 overall
+      |    0          2    18     7  ...   3  <--  first part1 for this subhalo is  7th part1 overall
+      |                              ...
+      |    0         -1   130    58  ...  33  <-- particles of halo-0, no subhalo
+      |
+      |    1         22   137    60  ...  35  <-- In halo-0 there were 22 subhalos and 137 part0, etc
+      |    1         23              ...
+      |                              ...
+      |                              ...
+      |   -1         -1  2020   988  ... 400
+      | ====================================
 
-Thus, given a particle5 of index 35, we know that that particle belongs to Halo-1, Subhalo-22.
-Alternatively, a particle0 index of 134 belongs to halo-0, and has no subhalo.
-Finally, any Particle1's with index 988 or after belong to no subhalo, and no halo.
+    Thus, given a particle5 of index 35, we know that that particle belongs to Halo-1, Subhalo-22.
+    Alternatively, a particle0 index of 134 belongs to halo-0, and has no subhalo.
+    Finally, any Particle1's with index 988 or after belong to no subhalo, and no halo.
 
 """
+
 
 import os
 import numpy as np
@@ -74,30 +72,32 @@ import zcode.InOut as zio
 
 import illustris_python as ill
 
-_VERSION = 0.3
+_VERSION = 0.4
 
 
-class OFFSET():
+class OFFTAB():
     """ Keys for offset table dictionary. """
 
-    RUN        = 'run'
-    SNAP       = 'snapshot'
-    VERSION    = 'version'
-    CREATED    = 'created'
-    FILENAME   = 'filename'
+    RUN         = 'run'
+    SNAP        = 'snapshot'
+    VERSION     = 'version'
+    CREATED     = 'created'
+    FILENAME    = 'filename'
 
-    HALOS      = 'halo_numbers'
-    SUBHALOS   = 'subhalo_numbers'
-    PARTICLES  = 'particle_numbers'
+    HALOS       = 'halo_numbers'
+    SUBHALOS    = 'subhalo_numbers'
+    OFFSETS     = 'particle_offsets'
 
-    BH_IDS     = 'bh_ids'
-    BH_INDICES = 'bh_indices'
+    BH_IDS      = 'bh_ids'
+    BH_INDICES  = 'bh_indices'
+    BH_HALOS    = 'bh_halos'
+    BH_SUBHALOS = 'bh_subhalos'
 
 
 
 _OFFSET_TABLE_FILENAME_BASE = "offsets/ill%d_snap%d_offset-table_v%.2f.npz"
 
-def GET_OFFSET_TABLE_FILENAME(run, snap, version=_VERSION):
+def _GET_OFFSET_TABLE_FILENAME(run, snap, version=_VERSION):
     fname  = GET_PROCESSED_DIR(run)
     fname += _OFFSET_TABLE_FILENAME_BASE % (run, snap, version)
     return fname
@@ -124,7 +124,7 @@ def loadOffsetTable(run, snap, loadsave=True, verbose=True):
 
     if( verbose ): print " - - HaloOffsetTable.loadOffsetTable()"
 
-    saveFile = GET_OFFSET_TABLE_FILENAME(run, snap)
+    saveFile = _GET_OFFSET_TABLE_FILENAME(run, snap)
 
     ## Load Existing Save
     #  ------------------
@@ -149,28 +149,38 @@ def loadOffsetTable(run, snap, loadsave=True, verbose=True):
         # Construct Offset Data
         haloNums, subhNums, offsets = _constructOffsetTable(run, snap, verbose=verbose)
 
-        # Construct index Data
+        # Construct BH index Data
         bhInds, bhIDs = _constructBHIndexTable(run, snap, verbose=verbose)
+
+        # Find BH Subhalos
+        binInds = np.digitize(bhInds, offsets[:,PARTICLE.BH]).astype(DTYPE.INDEX)-1
+        if( any(binInds < 0) ): raise RuntimeError("Some bhInds not matched!! '%s'" % (str(bads)))
+        bhHalos = haloNums[binInds]
+        bhSubhs = subhNums[binInds]
 
 
         offsetTable = {}
 
-        # Store data
-        offsetTable[OFFSET.HALOS]      = haloNums
-        offsetTable[OFFSET.SUBHALOS]   = subhNums
-        offsetTable[OFFSET.PARTICLES]  = offsets
-        offsetTable[OFFSET.BH_INDICES] = bhInds
-        offsetTable[OFFSET.BH_IDS]     = bhIDs
+        # Metadata
+        offsetTable[OFFTAB.RUN]         = run
+        offsetTable[OFFTAB.SNAP]        = snap
+        offsetTable[OFFTAB.VERSION]     = _VERSION
+        offsetTable[OFFTAB.CREATED]     = datetime.now().ctime()
+        offsetTable[OFFTAB.FILENAME]    = saveFile
 
-        # Add Metadata
-        offsetTable[OFFSET.RUN]        = run
-        offsetTable[OFFSET.SNAP]       = snap
-        offsetTable[OFFSET.VERSION]    = _VERSION
-        offsetTable[OFFSET.CREATED]    = datetime.now().ctime()
-        offsetTable[OFFSET.FILENAME]   = saveFile
+        # Offsets table data
+        offsetTable[OFFTAB.HALOS]       = haloNums
+        offsetTable[OFFTAB.SUBHALOS]    = subhNums
+        offsetTable[OFFTAB.OFFSETS]     = offsets
 
-        # Save
-        zio.dictToNPZ(offsetTable, saveFile)
+        # BH Specific data
+        offsetTable[OFFTAB.BH_INDICES]  = bhInds
+        offsetTable[OFFTAB.BH_IDS]      = bhIDs
+        offsetTable[OFFTAB.BH_HALOS]    = bhHalos
+        offsetTable[OFFTAB.BH_SUBHALOS] = bhSubhalos
+
+        # Save to file
+        zio.dictToNPZ(offsetTable, saveFile, verbose=verbose)
 
         stop = datetime.now()
         if( verbose ): print " - - - - Done after %s" % (str(stop-start))
@@ -180,149 +190,6 @@ def loadOffsetTable(run, snap, loadsave=True, verbose=True):
 
 # loadOffsetTable()
 
-
-
-def getBHSubhaloIndices(run, snap, bhIDs, verbose=True):
-    """
-    Retrieve the subhalo indices which host the given Blackhole ID numbers.
-
-    BHs who were not matched in the snapshot return indices of '-1', which can happen if
-    they are the 'in'-BH of a later Merger before the snapshot is written (for example).
-    The Subhalo indices corresponding to such missing BHs are also '-1'.
-
-    Arguments
-    ---------
-    run      : <int>,     illustris simulation number {1,3}
-    snap     : <int>,     illustris snapshot   number {0,135}
-    bhIDs    : <long>[N], Blackhole ID numbers
-    verbose  : <bool>,    (optional=VERBOSE) print verbose output
-
-    Returns
-    -------
-    bhInds   : <long>[N], array of Blackhole indices (in the snapshot files)
-    subhInds : <long>[N], array of Subhalo   indices (in the snapshot/groupfind files)
-
-    """
-
-
-    if( verbose ): print " - - HaloOffsetTable.getBHSubhaloIndices()"
-
-    # Load Offset Table
-    if( verbose ): print " - - - Loading offset table"
-    offsetTable = loadOffsetTable(run, snap, verbose=verbose)
-
-    # Convert from ID numbers to indices in snapshot files
-    if( verbose ): print " - - - Converting from particle IDs to particle Indices"
-    bhInds = _bhIDsToIndices(bhIDs, offsetTable, verbose=verbose)
-
-    # Get Hosts
-    haloHosts, subhHosts = _getHostsFromParticleIndex(run, snap, bhInds, verbose=verbose)
-
-    return bhInds, subhHosts
-
-# getBHSubhaloIndices()
-
-
-
-
-def _getHostsFromParticleIndex(run, snap, pind, verbose=True):
-    '''
-    Given the index of a particle, find the indices of the halo and subhalo it belongs to.
-
-    This method uses the offset tables calculated in an arepo.Subfind object to reverse out its
-    parent (FOF)halo(/group) and subhalo.
-
-    '''
-
-    PTYPE = PARTICLE_TYPE_BH
-
-    if( verbose ): print " - - HaloOffsetTable._getHostsFromParticleIndex()"
-
-    # Convert input indices to np.array
-    if( np.iterable(pind) ): bhInds = np.array( pind )
-    else:                    bhInds = np.array([pind])
-
-    if( verbose ):  print " - - - Searching for %d indices" % (len(bhInds))
-
-
-    # Load offset table
-    if( verbose ): print " - - - Loading offset table"
-    offsetTable = loadOffsetTable(run, snap, verbose=verbose)
-    haloNums = offsetTable[OFFSET.HALOS]
-    subhNums = offsetTable[OFFSET.SUBHALOS]
-    partNums = offsetTable[OFFSET.PARTICLES][:,PTYPE]
-    if( verbose ): print " - - - - Loaded %d entries" % (len(haloNums))
-
-
-    ### Find Host Halos and Subhalos ###
-
-    if( verbose ): print " - - - Finding halo bins"
-    # Find the entry for each index, np.digitize assigns values to right bin, shift left with '-1'
-    binInds = np.digitize(bhInds, partNums).astype(DTYPE.INDEX)-1
-
-    hostHalos = haloNums[binInds]
-    hostSubhs = subhNums[binInds]
-
-    # Some `bhInds` are not-found (equal '-1'), set results to also be null ('-1')
-    bads = np.where( binInds < 0 )[0]
-    if( len(bads) > 0 ):
-        hostHalos[bads] = -1
-        hostSubhs[bads] = -1
-
-
-    return hostHalos, hostSubhs
-
-# _getHostsFromParticleIndex()
-
-
-
-
-
-def _bhIDsToIndices(inIDs, table, verbose=True):
-    """
-    Convert from blackhole ID numbers to indexes using an offset table dictionary.
-
-    Notes
-    -----
-    Not all BH IDs will be found.  Incorrect/missing matches return '-1' elements
-
-    """
-
-    if( verbose ): print " - - HaloOffsetTable._idsToIndices()"
-
-    outIDs  = table[OFFSET.BH_IDS]
-    outInds = table[OFFSET.BH_INDICES]
-
-    ### Find Indices of Matches ###
-
-    # Sort IDs for faster searching
-    sortIDs = np.argsort(outIDs)
-    # Find matches in sorted array
-    foundSorted = np.searchsorted(outIDs, inIDs, sorter=sortIDs)
-    # Reverse map to find matches in original array
-    found = sortIDs[foundSorted]
-
-
-    foundIDs  = outIDs[found]
-    foundInds = outInds[found]
-
-
-    ### Check Matches ###
-
-    # Find incorrect matches
-    inds = np.where( inIDs != foundIDs )[0]
-    numBad = len(inds)
-    numGood = len(inIDs)-numBad
-    if( verbose ): print " - - - %d Good, %d Bad Matches" % (numGood, numBad)
-    # Set incorrect matches to '-1'
-    if( len(inds) > 0 ):
-        foundIDs[inds]  = -1
-        foundInds[inds] = -1
-
-
-    return foundInds
-
-# _bhIDsToIndices()
 
 
 def _constructOffsetTable(run, snap, verbose=True):
@@ -489,8 +356,5 @@ def _constructBHIndexTable(run, snap, verbose=True):
     return inds, bhIDs
 
 # _constructBHIndexTable()
-
-
-
 
 
