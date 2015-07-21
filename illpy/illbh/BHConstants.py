bef66d2f736aeb942ef0bd0f6a2e64b7871668e9
lzkelley <lkelley@cfa.harvard.edu>
2015-07-21 13:44:55 -0400

    Bug fix for assertions and added some docstrings

--- illpy/illbh/BHConstants.py
+++ illpy/illbh/BHConstants.py
@@ -1,17 +1,38 @@
 """
 Constants for Blackhole related functions and submodules.
 
+Classes
+-------
+    MERGERS : enum-type class for BH-Merger dictionary keys.
+              The list ``MERGERS_PHYSICAL_KEYS`` contains the keys which pertain to values taken
+              from the BH Merger files themselves
+    DETAILS : enum-type class for BH-Details entries dictionary keys.
+              The list ``DETAILS_PHYSICAL_KEYS`` contains the keys corresponding to values taken
+              from the BH Details files themselves
+    BH_TYPE : enum-type class for tracking the two types {``IN``,``OUT``} of Merger BHs.
+              The ``OUT`` BH is the one which persists after the merger, while the ``IN`` BH
+              effectively dissappears.
+    BH_TIME : enum-type class for the three stored, details times {``FIRST``,``BEFORE``,``AFTER``}.
+    BH_TREE : enum-type class for BH merger tree dictionary keys.
+
+
+Functions
+---------
+
+
 
 """
 
-
 import numpy as np
+from enum import Enum
 from glob import glob
 
 from .. Constants import NUM_SNAPS, GET_ILLUSTRIS_RUN_NAMES, _PROCESSED_DIR, GET_PROCESSED_DIR
 
 
-### Illustris Parameters ###
+## Illustris Parameters
+#  ====================
+
 _ILLUSTRIS_MERGERS_FILENAME_REGEX = "blackhole_mergers_*.txt"
 _ILLUSTRIS_DETAILS_FILENAME_REGEX = "blackhole_details_*.txt"
 
@@ -36,7 +57,8 @@ _ILLUSTRIS_DETAILS_DIRS = { 3 : "/n/ghernquist/Illustris/Runs/L75n455FP/output/b
                             }
 
 
-### Post-Processing Parameters ###
+## Post-Processing Parameters
+#  ==========================
 _PROCESSED_MERGERS_DIR          = _PROCESSED_DIR + "blackhole_mergers/"
 _PROCESSED_DETAILS_DIR          = _PROCESSED_DIR + "blackhole_details/"
 
@@ -53,7 +75,6 @@ _BLACKHOLE_TREE_FILENAME        = "ill-%d_bh-tree_v%.2f.npz"
 
 
 
-# Key Names for Mergers Dictionary
 class MERGERS(Enum):
     # Meta Data
     RUN       = 'run'
@@ -77,29 +98,6 @@ class MERGERS(Enum):
 MERGERS_PHYSICAL_KEYS = [ MERGERS.IDS, MERGERS.SCALES, MERGERS.MASSES ]
 
 
-
-
-class BH_TYPE(Enum):
-    IN  = 0
-    OUT = 1
-# } BH_TYPE
-
-assert BH_TYPE.IN == 0 and BH_TYPE.OUT == 1, \
-    "``BH_TYPE.{IN/OUT}`` MUST be in the proper order!"
-
-
-# Types of matches between mergers and details
-class BH_TIME(Enum):
-    BEFORE  = 0                                   # Before merger time (MUST = 0!)
-    AFTER   = 1                                   # After (or equal) merger time (MUST = 1!)
-    FIRST   = 2                                   # First matching details entry (MUST = 2!)
-# } BH_TIME
-
-assert BH_TIME.BEFORE == 0 and BH_TIME.AFTER == 1 and BH_TIME.FIRST == 2, \
-    "``BH_TIME.{BEFORE/AFTER/FIRST}`` MUST be in the proper order!"
-
-
-### Dictionary Keys for Details Parameters ###
 class DETAILS(Enum):
     RUN     = 'run'
     CREATED = 'created'
@@ -117,10 +115,21 @@ class DETAILS(Enum):
 
 # } DETAILS
 
-DETAILS_PHYSICAL_KEYS = [ DETAILS_IDS, DETAILS_SCALES, DETAILS_MASSES,  DETAILS_MDOTS, DETAILS_RHOS, DETAILS_CS ]
+DETAILS_PHYSICAL_KEYS = [ DETAILS.IDS, DETAILS.SCALES, DETAILS.MASSES,  
+                          DETAILS.MDOTS, DETAILS.RHOS, DETAILS.CS ]
 
 
-### BH Merger Tree ###
+class BH_TYPE(Enum):
+    IN  = 0
+    OUT = 1
+
+
+class BH_TIME(Enum):
+    BEFORE  = 0                                   # Before merger time (MUST = 0!)
+    AFTER   = 1                                   # After (or equal) merger time (MUST = 1!)
+    FIRST   = 2                                   # First matching details entry (MUST = 2!)
+
+
 class BH_TREE(Enum):
     LAST         = 'last'
     NEXT         = 'next'
@@ -134,13 +143,11 @@ class BH_TREE(Enum):
     RUN          = 'run'
     VERSION      = 'version'
 
-# } BH_TREE
+
 
 
 def GET_ILLUSTRIS_BH_MERGERS_FILENAMES(run, verbose=True):
-
     if( verbose ): print " - - BHConstants.GET_ILLUSTRIS_BH_MERGERS_FILENAMES()"
-
     filesDir = _ILLUSTRIS_MERGERS_DIRS[run]
     files = []
     if( type(filesDir) != list ): filesDir = [ filesDir ]
@@ -216,3 +223,12 @@ def GET_BLACKHOLE_TREE_FILENAME(run, version):
     fname = _PROCESSED_DIR % (GET_ILLUSTRIS_RUN_NAMES(run))
     fname += _BLACKHOLE_TREE_FILENAME % (run, version)
     return fname
+
+
+assert BH_TYPE.IN.value == 0 and BH_TYPE.OUT.value == 1, \
+    "``BH_TYPE.{IN/OUT}`` MUST be in the proper order!"
+
+
+assert BH_TIME.BEFORE.value == 0 and BH_TIME.AFTER.value == 1 and BH_TIME.FIRST.value == 2, \
+    "``BH_TIME.{BEFORE/AFTER/FIRST}`` MUST be in the proper order!"
+
