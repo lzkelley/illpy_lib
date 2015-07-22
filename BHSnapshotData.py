c7b50b1e4ea370c3e6400eb2df4e9efd717163f9
lzkelley <lkelley@cfa.harvard.edu>
2015-07-22 14:11:20 -0400

    Added in a bunch of MPI template stuff

--- BHSnapshotData.py
+++ BHSnapshotData.py
@@ -9,19 +9,20 @@ from datetime import datetime
 from enum import Enum
 import os, sys
 
+from mpi4py import MPI
+
 from illpy.Constants import NUM_SNAPS, GET_ILLUSTRIS_OUTPUT_DIR
 from illpy.illbh import BHMergers
 from illpy.illbh.BHConstants import MERGERS, BH_TYPE
 
-from Settings import *
+import Settings
 
 import illustris_python as ill
 
 import zcode
 import zcode.InOut as zio
 
-
-VERSION = 0.2
+_VERSION = 0.2
 
 _SAVE_FILE_NAME = "./data/ill-%d_bh_snapshot_data_v-%.1f.npz"
 def GET_SAVE_FILE_NAME(run, vers): return _SAVE_FILE_NAME % (run, vers)
@@ -35,14 +36,78 @@ class SNAP():
 SNAPSHOT_FIELDS = ['ParticleIDs', 'BH_Hsml', 'BH_Mass', 'Masses', 'SubfindHsml']
 
 
+class TAGS():
+    READY = 0
+    START = 1
+    DONE  = 2
+    EXIT  = 3
+# } class TAGS
+
+
+class ENVSTAT():
+    FAIL = -1
+    EXST =  0
+    NEWF =  1
+# } class ENVSTAT
+
+
 
 def main():
 
-    if( len(sys.argv) > 1 ): run = np.int(sys.argv[1])
-    else:                    run = RUN_NUM
+    ## Initialize MPI Parameters
+    #  -------------------------
 
-    print " - BHSnapshotData.main()"
-    print " - - Loading BH Snapshot Data for run %d" % (run)
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    size = comm.size
+    name = MPI.Get_processor_name()
+    stat = MPI.Status()
+
+    if( rank == 0 ):
+        NAME = sys.argv[0]
+        print "\n%s\n%s\n%s" % (NAME, '='*len(NAME), str(datetime.now()))
+
+
+    ## Parse Arguments
+    #  ---------------
+    args    = _parseArguments()
+    run     = args.run
+    verbose = args.verbose
+
+    ## Master Process
+    #  --------------
+    if( rank == 0 ):
+        print "RUN           = %d  " % (run)
+        print "VERSION       = %.2f" % (_VERSION)
+        print "MPI COMM SIZE = %d  " % (size)
+        print ""
+        print "VERBOSE       = %s  " % (str(verbose))
+        print ""
+        beg_all = datetime.now()
+
+        try: 
+            _runMaster(run, comm)
+        except Exception as err:
+            _mpiError(comm, err)
+
+        end_all = datetime.now()
+        print " - - Total Duration '%s'" % (str(end_all-beg_all))
+
+
+    ## Slave Processes
+    #  ---------------
+    else:
+
+        try:    
+            _runSlave(run, comm, verbose=True)
+        except Exception as err:
+            _mpiError(comm, err)
+
+            
+    return 
+
+
+
     beg = datetime.now()
     loadBHSnapshotData(run, verbose=True)
     end = datetime.now()
@@ -51,6 +116,195 @@ def main():
     return
 
 # main()
+
+
+
+def _runMaster(run, comm):
+    """
+    Run master process which manages all of the secondary ``slave`` processes.
+
+    Details
+    -------
+
+    """
+
+    stat = MPI.Status()
+    rank = comm.rank
+    size = comm.size
+
+    print " - Initializing"
+
+    mergSnap, snapMerg, mergSubh = getMergerAndSubhaloIndices(run, verbose=True)
+
+    # Get all subhalos for each snapshot (including duplicates and missing)
+    snapSubh     = [ mergSubh[smrg] for smrg in snapMerg ]
+    # Get unique subhalos for each snapshot, discard duplicates
+    snapSubh_uni = [ np.array(list(set(ssubh))) for ssubh in snapSubh ]
+    # Discard missing matches ('-1')
+    snapSubh_uni = [ ssubh[np.where(ssubh != -1)] for ssubh in snapSubh_uni ]
+
+    numUni = [len(ssubh) for ssubh in snapSubh_uni]
+    numUniTot = np.sum(numUni)
+    numMSnaps = np.count_nonzero(numUni)
+
+    print " - - %d Unique subhalos over %d Snapshots" % (numUniTot, numMSnaps)
+
+    ## Iterate over Snapshots and Subhalos
+    #  ===================================
+    #     distribute tasks to slave processes
+    
+    count = 0
+    new   = 0
+    exist = 0
+    fail  = 0
+    times = np.zeros(numUniTot)
+
+    statFileName = GET_ENVIRONMENTS_STATUS_FILENAME(run)
+    statFile = open(statFileName, 'w')
+    print " - - Opened status file '%s'" % (statFileName)
+    statFile.write('%s\n' % (str(datetime.now())))
+    beg = datetime.now()
+
+    for snap,subs in zmath.renumerate(snapSubh_uni):
+
+        if( len(subs) <= 0 ): continue
+
+        # Create output directory (subhalo doesn't matter since only creating dir)
+        #    don't let slave processes create it - makes conflicts
+        fname = GET_MERGER_SUBHALO_FILENAME(run, snap, 0)
+        zio.checkPath(fname)
+
+        # Get most bound particles for each subhalo in this snapshot
+        mostBound = Subhalo.importGroupCatalogData(run, snap, subhalos=subs, 
+                                                   fields=[SUBHALO.MOST_BOUND], verbose=False)
+
+        # Go over each subhalo
+        for boundID, subhalo in zip(mostBound, subs):
+
+            # Write status to file
+            dur = (datetime.now()-beg)
+            statStr = 'Snap %3d   %8d/%8d = %.4f   in %s   %8d new   %8d exist  %8d fail\n' % \
+                (snap, count, numUniTot, 1.0*count/numUniTot, str(dur), new, exist, fail)
+            statFile.write(statStr)
+            statFile.flush()
+
+            # Look for available slave process
+            data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=stat)
+            source = stat.Get_source()
+            tag = stat.Get_tag()
+
+            # Track number of completed profiles
+            if( tag == TAGS.DONE ): 
+                retStat, durat = data
+
+                times[count] = durat
+                count += 1
+                if(   retStat == ENVSTAT.NEWF ): new   += 1
+                elif( retStat == ENVSTAT.EXST ): exist += 1
+                else:                            fail  += 1
+
+
+            # Distribute tasks
+            comm.send([snap, subhalo, boundID], dest=source, tag=TAGS.START)
+
+        # } for boundID, subhalo 
+
+    # } for snap, subs
+
+    statFile.write('\n\nDone after %s' % (str(datetime.now()-beg)))
+    statFile.close()
+
+    ## Close out all Processes
+    #  =======================
+
+    numActive = size-1
+    print " - Exiting %d active processes" % (numActive)
+    while( numActive > 0 ):
+        
+        # Find available slave process
+        data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=stat)
+        source = stat.Get_source()
+        tag = stat.Get_tag()
+
+        # If we're recieving exit confirmation, count it
+        if( tag == TAGS.EXIT ): numActive -= 1
+        else:
+            # If a process just completed, count it
+            if( tag == TAGS.DONE ): 
+                times[count] = data[1]
+                count += 1
+                if( data[0] ): new += 1
+
+            # Send exit command
+            comm.send(None, dest=source, tag=TAGS.EXIT)
+
+
+
+    print " - - %d/%d = %.4f Completed tasks!" % (count, numUniTot, 1.0*count/numUniTot)
+    print " - - %d New Files" % (new)
+
+    return
+    
+# _runMaster()
+
+
+
+
+def _runSlave(run, comm, radBins=None, loadsave=True, verbose=False):
+    """
+
+    Arguments
+    ---------
+       run      <int>       : illustris simulation run number {1,3}
+       comm     <...>       : MPI intracommunicator object (e.g. ``MPI.COMM_WORLD``)
+
+       loadsave <bool>      : optional, load data for this subhalo if it already exists
+
+    Details
+    -------
+     - Waits for ``master`` process to send subhalo numbers
+
+     - Returns status to ``master``
+
+    """
+
+    stat = MPI.Status()
+    rank = comm.rank
+    size = comm.size
+
+    if( verbose ): print " - - Environments._runSlave() : rank %d/%d" % (rank, size)
+
+    # Keep looking for tasks until told to exit
+    while True:
+        # Tell Master this process is ready
+        comm.send(None, dest=0, tag=TAGS.READY)
+        # Receive ``task`` ([snap,boundID,subhalo])
+        task = comm.recv(source=0, tag=MPI.ANY_TAG, status=stat)
+        tag = stat.Get_tag()
+
+        if( tag == TAGS.START ):
+            # Extract parameters of environment
+            snap, subhalo, boundID = task
+            beg = datetime.now()
+            # Load and save Merger Environment
+            retEnv, retStat = loadMergerEnv(run, snap, subhalo, boundID, radBins=radBins, 
+                                            loadsave=True, verbose=verbose)
+            end = datetime.now()
+            durat = (end-beg).total_seconds()
+            comm.send([retStat,durat], dest=0, tag=TAGS.DONE)
+        elif( tag == TAGS.EXIT  ):
+            break
+
+
+    # Finish, return done
+    comm.send(None, dest=0, tag=TAGS.EXIT)
+
+    return
+    
+# _runSlave()
+
+
+
 
 
 def loadBHSnapshotData(run, loadsave=True, verbose=True, debug=False):
@@ -78,7 +332,7 @@ def loadBHSnapshotData(run, loadsave=True, verbose=True, debug=False):
 
     if( verbose ): print " - - BHSnapshotData.loadBHSnapshotData()"
     
-    saveFileName = GET_SAVE_FILE_NAME(run, VERSION)
+    saveFileName = GET_SAVE_FILE_NAME(run, _VERSION)
 
     ## Load Existing Data
     if( loadsave ):
@@ -87,7 +341,7 @@ def loadBHSnapshotData(run, loadsave=True, verbose=True, debug=False):
             if( verbose ): print " - - - Snapshot Data save file '%s' Exists" % (saveFileName)
             snapData = zio.npzToDict(saveFileName)
             # Make sure version matches
-            if( snapData[SNAP.VERSION] == VERSION ):
+            if( snapData[SNAP.VERSION] == _VERSION ):
                 if( verbose ): print " - - - Snapshot Particle Data loaded"
             else:
                 print " - - - Snapshot Data save file '%s' is out of date!" % (saveFileName)
@@ -133,7 +387,7 @@ def _importBHSnapshotData(run, verbose=True, debug=False):
     if( verbose ): print " - - BHSnapshotData._importBHSnapshotData()"
 
     dir_illustris = GET_ILLUSTRIS_OUTPUT_DIR(run)
-    if( verbose ): print " - - - Using illustirs data dir '%s'" % (dir_illustris)
+    if( verbose ): print " - - - Using illustris data dir '%s'" % (dir_illustris)
 
     ## Load BH Mergers
     #  ===============
@@ -217,7 +471,7 @@ def _importBHSnapshotData(run, verbose=True, debug=False):
     if( verbose ): print " - - - - %d Good, %d Bad - after %s" % (num_pos, num_neg, str(end-beg))
 
     # Add Meta-Data
-    data[SNAP.VERSION] = VERSION
+    data[SNAP.VERSION] = _VERSION
     data[SNAP.RUN]     = run
     data[SNAP.CREATED] = str(datetime.now())
     data[SNAP.DIR_SRC] = dir_illustris
@@ -225,6 +479,60 @@ def _importBHSnapshotData(run, verbose=True, debug=False):
     return data
     
 # _importBHSnapshotData()
+
+
+
+def _parseArguments():
+    """
+    Prepare argument parser and load command line arguments.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version='%s %.2f' % (sys.argv[0], _VERSION))
+    parser.add_argument('-v', '--verbose', action='store_true', 
+                        help='Verbose output', default=Settings.VERBOSE)
+
+    '''
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--check",    dest='check',   action="store_true", default=CHECK_EXISTS)
+    group.add_argument("--no-check", dest='nocheck', action="store_true", default=(not CHECK_EXISTS))
+    '''
+    parser.add_argument("run", type=int, nargs='?', choices=[1, 2, 3],
+                        help="illustris simulation number", default=Settings.RUN_NUM)
+    args = parser.parse_args()
+    
+    return args
+
+# _parseArguments()
+
+
+
+def _mpiError(comm, err="ERROR"):
+    """
+    Raise an error through MPI and exit all processes.
+
+    Arguments
+    ---------
+       comm <...> : mpi intracommunicator object (e.g. ``MPI.COMM_WORLD``)
+       err  <str> : optional, extra error-string to print
+
+    """
+
+    import traceback
+    rank = comm.rank
+
+    print "\nERROR: rank %d\n%s\n" % (rank, str(datetime.now()))
+    print sys.exc_info()[0]
+    print err.message
+    print err.__doc__
+    print "\n"
+    print(traceback.format_exc())
+    print "\n\n"
+
+    comm.Abort(rank)
+    return
+
+# _mpiError()
 
 
 
