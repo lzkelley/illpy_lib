fd4c1825c6ff71dfdf7e45a28f7faa417c4d116b
lzkelley <lkelley@cfa.harvard.edu>
2015-11-09 16:33:44 -0500

    Minor: cleanup

--- illpy/__init__.py
+++ illpy/__init__.py
@@ -1,16 +1,5 @@
+"""Illustris simulation data manipulation routines and submodules.
 """
-
-"""
-
-__author__ = "Luke Zoltan Kelley"
-__version__ = "0.1"
-__email__ = "lkelley@cfa.harvard.edu"
-__status__ = "Development"
-
 
 import Constants
-#import illcosmo
 from illcosmo.Cosmology import Cosmology
-#import AuxFuncs
-#import illbh
-#import Subhalos
