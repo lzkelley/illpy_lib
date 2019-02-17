3a495edee22f92a18a35e3bd1d1c646f5e84f769
lzkelley <lkelley@cfa.harvard.edu>
2019-02-17 15:23:15 -0500

    Add details filenames to 'Paths' class

--- illpy_lib/illbh/__init__.py
+++ illpy_lib/illbh/__init__.py
@@ -3,8 +3,6 @@
 import os
 import glob
 
-import os
-
 # import numpy as np
 
 import pycore
@@ -13,8 +11,10 @@ import pycore
 class Settings(pycore.Settings):
 
     # NAME = "fire-mergers"
-    VERBOSITY = 30
+    # VERBOSITY = 30
+    VERBOSITY = 20
     LOG_FILENAME = "log_illpy-lib_bh.log"
+    RUN_NUM = 1
 
     INPUT = "/n/ghernquist/Illustris/Runs/L75n1820FP/"
     OUTPUT = "/n/regal/hernquist_lab/lkelley/illustris-processed/"
@@ -38,6 +38,12 @@ class Paths(pycore.Paths):
     FNAME_DETAILS_CLEAN = "bh_details.hdf5"
     FNAME_MERGERS_CLEAN = "bh_mergers.hdf5"
 
+    # "ill-%d_blackhole_details_temp_snap-%d.txt"
+    FNAME_DETAILS_TEMP_SNAP = "ill-{run_num:d}_blackhole_details_temp_snap-{snap_num:03d}.txt"
+
+    # "ill-%d_blackhole_details_save_snap-%d_v%.2f.npz"
+    FNAME_DETAILS_SNAP = "ill-{run_num:d}_blackhole_details_snap-{snap_num:03d}.hdf5"
+
     # The substituted string should be either 'mergers' or 'details'
     _ILL_1_TXT_DIRS = [
         "txt-files-curie/blackhole_{}/",
@@ -59,7 +65,7 @@ class Paths(pycore.Paths):
         return self._find_input_files('mergers', self._MERGERS_FILENAME_REGEX)
 
     @property
-    def details_input(self):
+    def fnames_details_input(self):
         return self._find_input_files('details', self._DETAILS_FILENAME_REGEX)
 
     def _find_input_files(self, name, regex):
@@ -72,7 +78,7 @@ class Paths(pycore.Paths):
         elif ('illustris-2' in self.INPUT.lower()) or ('L75n910FP' in self.INTPUT):
             log.debug("Input looks like `illustris-2` ('{}')".format(self.INPUT))
             # subdir = "/combined_output/blackhole_mergers/"
-            subdir = "/combined_output/blackhole_{}/".format(name)            
+            subdir = "/combined_output/blackhole_{}/".format(name)
             paths = [os.path.join(self.INPUT, subdir)]
         else:
             log.debug("Input looks like `illustris-3` or default ('{}')".format(
@@ -80,7 +86,7 @@ class Paths(pycore.Paths):
             # subdir = "/output/blackhole_mergers/"
             subdir = "/output/blackhole_{}/".format(name)
             paths = [os.path.join(self.INPUT, subdir)]
-        
+
         files = []
         log.debug("Checking {} directories for {} files".format(len(paths), name))
         for pp in paths:
@@ -96,11 +102,15 @@ class Paths(pycore.Paths):
             files += _fils
 
         log.debug("Found {} {} files".format(len(files), name))
-        return files        
+        return files
 
     @property
     def details_clean(self):
         return os.path.join(self.OUTPUT, self.FNAME_DETAILS_CLEAN)
+
+    @property
+    def output_details(self):
+        return os.path.join(self.OUTPUT, "details", "")
 
     @property
     def mergers_clean(self):
@@ -112,6 +122,22 @@ class Paths(pycore.Paths):
         # path = os.path.realpath(path)
         path = self.check_path(path)
         return path
+
+    def fname_details_temp_snap(self, snap, run_num=None):
+        if run_num is None:
+            run_num = self._core.sets.RUN_NUM
+
+        fname = self.FNAME_DETAILS_TEMP_SNAP.format(snap_num=snap, run_num=run_num)
+        fname = os.path.join(self.output_details, fname)
+        return fname
+
+    def fname_details_snap(self, snap, run_num=None):
+        if run_num is None:
+            run_num = self._core.sets.RUN_NUM
+
+        fname = self.FNAME_DETAILS_SNAP.format(snap_num=snap, run_num=run_num)
+        fname = os.path.join(self.output_details, fname)
+        return fname
 
 
 class Core(pycore.Core):
