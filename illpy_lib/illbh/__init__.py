b9bc4afa0da7c89377a150d37145fdd008c07309
lzkelley <lkelley@cfa.harvard.edu>
2019-03-07 11:57:39 -0500

    Move methods from '__init__' to new file 'utils.py'

--- illpy_lib/illbh/__init__.py
+++ illpy_lib/illbh/__init__.py
@@ -293,26 +293,6 @@ class Core(pycore.Core):
         return cosmo
 
 
-def load_hdf5_to_mem(fname):
-    with h5py.File(fname, 'r') as data:
-        out = {kk: data[kk][:] for kk in data.keys()}
-    return out
-
-
-def _distribute_snapshots(comm):
-    """Evenly distribute snapshot numbers across multiple processors.
-    """
-    size = comm.size
-    rank = comm.rank
-    mySnaps = np.arange(NUM_SNAPS)
-    if size > 1:
-        # Randomize which snapshots go to which processor for load-balancing
-        mySnaps = np.random.permutation(mySnaps)
-        # Make sure all ranks are synchronized on initial (randomized) list before splitting
-        mySnaps = comm.bcast(mySnaps, root=0)
-        mySnaps = np.array_split(mySnaps, size)[rank]
-
-    return mySnaps
-
+from . utils import load_hdf5_to_mem, _distribute_snapshots  # noqa
 
 # from . import bh_constants  # noqa
---
+++ illpy_lib/illbh/utils.py
@@ -0,0 +1,32 @@
+"""
+"""
+
+import numpy as np
+import h5py
+
+from illpy_lib.illbh import NUM_SNAPS
+
+
+def load_hdf5_to_mem(fname):
+    with h5py.File(fname, 'r') as data:
+        # out = {kk: data[kk][()] if np.shape(data[kk]) == () else data[kk][:] for kk in data.keys()}
+        # use `[()]` instead of `[:]` to handle scalar datasets
+        out = {kk: data[kk][()] for kk in data.keys()}
+
+    return out
+
+
+def _distribute_snapshots(comm):
+    """Evenly distribute snapshot numbers across multiple processors.
+    """
+    size = comm.size
+    rank = comm.rank
+    mySnaps = np.arange(NUM_SNAPS)
+    if size > 1:
+        # Randomize which snapshots go to which processor for load-balancing
+        mySnaps = np.random.permutation(mySnaps)
+        # Make sure all ranks are synchronized on initial (randomized) list before splitting
+        mySnaps = comm.bcast(mySnaps, root=0)
+        mySnaps = np.array_split(mySnaps, size)[rank]
+
+    return mySnaps
