a31e7fe1081f7ce4282dddf7c33b7dfc004d84b7
Luke Zoltan Kelley <lzkelley@gmail.com>
2020-08-09 17:10:21 -0400

    Restructure cosmology class to load parameters from simulation directory (instead of being hardcoded)

--- illpy_lib/illcosmo.py
+++ illpy_lib/illcosmo.py
@@ -1,10 +1,18 @@
 """
 
+TO-DO
+  - 2020-08-06 : If usedvalues (parameters) file cannot be found, load from snapshots (headers).
+                 Check parameter values against snapshot values.
+
 """
 
-from collections import OrderedDict
 import os
+import logging
+import glob
+from collections import OrderedDict
+
 import numpy as np
+import h5py
 
 import cosmopy
 
@@ -15,6 +23,169 @@ _DATA_PATH = "%s/data/" % os.path.dirname(os.path.abspath(__file__))
 # Contains cosmological values for each snapshot
 _TIMES_FILENAME_TOS = "illustris-snapshot-cosmology-data.npz"
 _TIMES_FILENAME_TNG = "illustris-tng_snapshot-cosmology-data.npz"
+
+
+class Simulation_Cosmology(cosmopy.Cosmology):
+
+    Omega0 = None
+    OmegaLambda = None
+    OmegaBaryon = None
+    HPAR = None
+    H0 = None
+
+    BOX_LENGTH_COM_MPC = None
+    BOX_LENGTH_COM_CM = None
+    BOX_VOLUME_COM_MPC3 = None
+    BOX_VOLUME_COM_CM3 = None
+
+    NUM_SNAPS = None
+    SCALES = None
+
+    class CONV_SIM_TO_CGS:
+        """Convert from simulation units to physical [cgs] units (multiply).
+        """
+        DIST = None
+        MASS = None
+        VEL  = None
+        MDOT = None
+        DENS = None
+
+    _Z_GRID = [10.0, 4.0, 2.0, 1.0, 0.5, 0.1, 0.02]
+    _INTERP_POINTS = 40
+
+    def __init__(self, sim_path, fname_params='param.txt-usedvalues', verbose=True, **kwargs):
+        self._verbose = verbose
+        self._sim_path = sim_path
+        if not os.path.isdir(sim_path):
+            raise ValueError("Simulation path '{}' does not exist!".format(sim_path))
+
+        # -- Load 'usedvalues' parameters file from Arepo simulation directory
+        _fnames = [fname_params, os.path.join(sim_path, fname_params)]
+        params = None
+        for fn in _fnames:
+            try:
+                params = self._load_used_params(fn)
+                if verbose:
+                    print("Loaded parameters from '{}'".format(fn))
+                    break
+            except FileNotFoundError:
+                pass
+
+        else:
+            raise FileNotFoundError("File '{}' does not exist!".format(fname_params))
+
+        self._params = params
+
+        # -- Extract the target values from the parameters
+        hpar = params['HubbleParam']
+        # Get units
+        mass = params['UnitMass_in_g'] / hpar
+        dist = params['UnitLength_in_cm'] / hpar
+        vel = params['UnitVelocity_in_cm_per_s']
+        dens = mass / np.power(dist, 3.0)
+        time = dist / vel
+        mdot = mass / time
+
+        # Get cosmological parameters
+        cosmo_pars = ['Omega0', 'OmegaLambda', 'OmegaBaryon', ]
+        for cp in cosmo_pars:
+            kwargs.setdefault(cp, params[cp])
+
+        kwargs.setdefault('HPAR', hpar)
+        kwargs.setdefault('H0', hpar * 100.0)
+        box_cm = params['BoxSize'] * dist / hpar
+        box_mpc = box_cm / MPC
+        kwargs.setdefault('BOX_LENGTH_COM_CM', box_cm)
+        kwargs.setdefault('BOX_LENGTH_COM_MPC', box_mpc)
+        kwargs.setdefault('BOX_VOLUME_COM_CM3', np.power(box_cm, 3.0))
+        kwargs.setdefault('BOX_VOLUME_COM_MPC3', np.power(box_mpc, 3.0))
+
+        self.CONV_SIM_TO_CGS.DIST = dist
+        self.CONV_SIM_TO_CGS.MASS = mass
+        self.CONV_SIM_TO_CGS.VEL = vel
+        self.CONV_SIM_TO_CGS.MDOT = mdot
+        self.CONV_SIM_TO_CGS.DENS = dens
+
+        for kk, vv in kwargs.items():
+            if not hasattr(self, kk):
+                raise ValueError("Additional `kwargs` must already be defined in class definition!")
+            setattr(self, kk, vv)
+
+        super().__init__()
+        self._load_snapshot_info()
+        return
+
+    def _load_used_params(self, fname):
+        params = OrderedDict()
+        with open(fname, 'r') as load:
+            for line in load.readlines():
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+
+                kk, vv = line.split()
+                for tt in [np.int, np.float]:
+                    try:
+                        vv = tt(vv)
+                    except ValueError:
+                        pass
+                    else:
+                        break
+                else:
+                    vv = str(vv)
+
+                try:
+                    params[kk] = vv
+                except:
+                    print(f"kk = {kk} ({type(kk)}), vv = {vv} ({type(vv)})")
+                    raise
+
+        return params
+
+    def _load_snapshot_info(self):
+        path = self._sim_path
+        path_output = os.path.join(path, "output", "")
+
+        snap_dirs = sorted(glob.glob(os.path.join(path_output, "snapdir_*")))
+        self.NUM_SNAPS = len(snap_dirs)
+        if self._verbose:
+            print("Found {} snapshot directories".format(self.NUM_SNAPS))
+
+        if self.NUM_SNAPS < 1:
+            logging.warning("WARNING: no snapshot directories found in '{}'".format(path_output))
+            return
+
+        scale = []
+        redz = []
+        for snap in range(self.NUM_SNAPS):
+            dir = snap_dirs[snap]
+            if not dir.endswith('_{:03d}'.format(snap)):
+                err = "WARNING: path for snapshot {} does not match index {}".format(dir, snap)
+                logging.warning(err)
+
+            fname_snap = glob.glob(os.path.join(dir, '*.hdf5'))
+            if len(fname_snap) == 0:
+                raise ValueError("No data files found in snapshot directory '{}'".format(dir))
+            fname_snap = fname_snap[0]
+            if self._verbose and snap == 0:
+                print("Snap {}: loading header info from '{}'".format(snap, fname_snap))
+            with h5py.File(fname_snap, 'r') as info:
+                head = info['Header']
+
+                aa = head.attrs['Time']
+                zz = head.attrs['Redshift']
+                if len(scale) > 0:
+                    if (aa <= scale[-1]) or (zz >= redz[-1]):
+                        err = "ERROR: snap {} a={},z={}  prev a={},z={}".format(
+                            aa, zz, scale[-1], redz[-1])
+                        raise ValueError(err)
+
+                scale.append(aa)
+                redz.append(zz)
+
+        self.scale = np.asarray(scale)
+        self.redz = np.asarray(redz)
+        return
 
 
 class _Illustris_Cosmology(cosmopy.Cosmology):
@@ -207,31 +378,3 @@ def cosmo_from_path(sim_path):
     cosmo._usedparams = params
 
     return cosmo
-
-
-def _load_used_params(fname):
-    params = OrderedDict()
-    with open(fname, 'r') as load:
-        for line in load.readlines():
-            line = line.strip()
-            if len(line) == 0:
-                continue
-
-            kk, vv = line.split()
-            for tt in [np.int, np.float]:
-                try:
-                    vv = tt(vv)
-                except ValueError:
-                    pass
-                else:
-                    break
-            else:
-                vv = str(vv)
-
-            try:
-                params[kk] = vv
-            except:
-                print(f"kk = {kk} ({type(kk)}), vv = {vv} ({type(vv)})")
-                raise
-        
-    return params
